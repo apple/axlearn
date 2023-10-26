2752701f56ff61534a8c0ea9cca804443f39056e
Mark Lee <markblee@apple.com>
2023-10-25 22:21:04 -0400

    Adds a simple LogMelFrontend. (#138)
    * Adds a simple LogMelFrontend.
    
    * Address Ruoming's comments.

---
+++ axlearn/audio/frontend.py
@@ -0,0 +1,127 @@
+# Copyright © 2023 Apple Inc.
+
+"""Audio frontends for feature extraction."""
+
+from typing import Callable, Dict, Optional, Sequence, Union
+
+import jax.numpy as jnp
+
+from axlearn.audio.frontend_utils import (
+    WindowType,
+    linear_to_log_mel_spectrogram,
+    linear_to_mel_weight_matrix,
+    magnitude_spectrogram,
+    next_power_of_2,
+    pre_emphasis,
+    sliding_window,
+    windowing,
+)
+from axlearn.common.base_layer import BaseLayer
+from axlearn.common.config import (
+    REQUIRED,
+    InstantiableConfig,
+    Required,
+    config_class,
+    maybe_instantiate,
+)
+from axlearn.common.module import Module
+from axlearn.common.utils import Tensor
+
+
+def normalize_by_mean_std(
+    x: Tensor, *, mean: Optional[Sequence[float]] = None, std: Optional[Sequence[float]] = None
+) -> Tensor:
+    """Scales the input by subtracting pre-computed `mean` and/or dividing by pre-computed `std`."""
+    if mean is not None:
+        x = x - jnp.array(mean, dtype=x.dtype)
+    if std is not None:
+        x = x / jnp.maximum(jnp.array(std, dtype=x.dtype), jnp.finfo(x.dtype).eps)
+    return x
+
+
+def _ms_to_samples(ms: Union[int, float], *, sample_rate: int) -> float:
+    """Converts time in milliseconds to number of samples under the given sample rate."""
+    return sample_rate / 1000 * ms
+
+
+class LogMelFrontend(BaseLayer):
+    """Computes Log Mel spectrogram features.
+
+    The frontend implements the following stages:
+        `Framer -> PreEmphasis -> Window -> FFT -> FilterBank -> MeanStdDev`.
+    """
+
+    @config_class
+    class Config(BaseLayer.Config):
+        """Configures LogMelFrontend."""
+
+        # Number of filters/bands in the output spectrogram.
+        num_filters: Required[int] = REQUIRED
+        # Number of input samples per second, e.g., 24000 for 24KHz inputs.
+        sample_rate: Required[int] = REQUIRED
+        # Size of each frame in ms.
+        frame_size_ms: Required[float] = REQUIRED
+        # Hop size in ms.
+        hop_size_ms: Required[float] = REQUIRED
+        # Optional output transformation. See `normalize_by_mean_std` for an example.
+        output_transformation: Optional[InstantiableConfig[Callable[[Tensor], Tensor]]] = None
+
+    def __init__(self, cfg: Config, *, parent: Optional[Module]):
+        super().__init__(cfg, parent=parent)
+        cfg = self.config
+        frame_size = _ms_to_samples(cfg.frame_size_ms, sample_rate=cfg.sample_rate)
+        hop_size = _ms_to_samples(cfg.hop_size_ms, sample_rate=cfg.sample_rate)
+        if not frame_size.is_integer():
+            raise ValueError(f"frame_size must be an integer, got {frame_size}.")
+        if not hop_size.is_integer():
+            raise ValueError(f"hop_size must be an integer, got {hop_size}.")
+
+        self._frame_size = int(frame_size)
+        self._hop_size = int(hop_size)
+
+        self._output_transformation = None
+        if cfg.output_transformation is not None:
+            self._output_transformation = maybe_instantiate(cfg.output_transformation)
+
+        # Mel filterbank, used to convert magnitude spectrogram to mel spectrogram. Only needs to be
+        # constructed once.
+        self._fft_size = next_power_of_2(self._frame_size)
+        self._filterbank = linear_to_mel_weight_matrix(
+            num_filters=cfg.num_filters,
+            num_spectrogram_bins=self._fft_size // 2 + 1,
+            sample_rate=cfg.sample_rate,
+            lower_edge_hertz=125.0,
+            upper_edge_hertz=7600.0,
+        )
+
+    def forward(self, inputs: Tensor, *, paddings: Tensor) -> Dict[str, Tensor]:
+        """Computes log-mel spectrogram features.
+
+        Args:
+            inputs: Tensor of dtype float32 and shape [batch, seq_len].
+            paddings: 0/1 tensor of shape [batch, seq_len]. 1's represent padded positions.
+
+        Returns:
+            A dict containing:
+            - outputs: of shape [batch, num_frames, num_filters, 1].
+            - paddings: 0/1 tensor of shape [batch, num_frames].
+        """
+        # TODO(markblee): Make these configurable as needed.
+        # Framer. Add 1 to frame size for pre-emphasis.
+        frames = sliding_window(inputs, window_size=self._frame_size + 1, stride=self._hop_size)
+        # Pre-emphasis filter.
+        frames = pre_emphasis(frames, coeff=0.97)
+        # Windowing. Defaults to a Hann window.
+        frames = windowing(frames, window_type=WindowType.HANN)
+        # FFT.
+        spectrogram = magnitude_spectrogram(frames, fft_size=self._fft_size)
+        # Convert to log-mel. [batch, num_frames, num_filters].
+        outputs = linear_to_log_mel_spectrogram(spectrogram, weight_matrix=self._filterbank)
+        if self._output_transformation is not None:
+            outputs = self._output_transformation(outputs)
+        # To identify padding frames, apply the framer to the input padding.
+        # Consider a frame padded if it contains at least one padding sample.
+        paddings = sliding_window(paddings, window_size=self._frame_size + 1, stride=self._hop_size)
+        paddings = jnp.max(paddings, axis=-1, keepdims=True)
+        outputs = outputs * (1 - paddings)
+        return dict(outputs=outputs[..., None], paddings=jnp.squeeze(paddings, axis=-1))
---
+++ axlearn/audio/frontend_test.py
@@ -0,0 +1,177 @@
+# Copyright © 2023 Apple Inc.
+
+"""Tests audio frontends."""
+
+import functools
+import math
+
+import jax
+import jax.numpy as jnp
+import pytest
+import tensorflow as tf
+from absl.testing import parameterized
+
+from axlearn.audio.frontend import LogMelFrontend, _ms_to_samples, normalize_by_mean_std
+from axlearn.audio.frontend_utils_test import (
+    _fake_audio,
+    _ref_framer,
+    _ref_log_mel_spectrogram,
+    _ref_pre_emphasis,
+)
+from axlearn.common.config import config_for_function
+from axlearn.common.module import functional as F
+
+
+class LogMelFrontendTest(parameterized.TestCase, tf.test.TestCase):
+    """Tests LogMelFrontend."""
+
+    @parameterized.parameters(
+        dict(frame_size_ms=3.90625, hop_size_ms=1),
+        dict(frame_size_ms=1, hop_size_ms=0.15625),
+    )
+    def test_instantiate(self, frame_size_ms, hop_size_ms):
+        num_filters, sample_rate = 80, 16_000
+        cfg = LogMelFrontend.default_config().set(
+            num_filters=num_filters,
+            sample_rate=sample_rate,
+            frame_size_ms=frame_size_ms,
+            hop_size_ms=hop_size_ms,
+        )
+        with self.assertRaisesRegex(ValueError, "must be an integer"):
+            cfg.set(name="test").instantiate(parent=None)
+
+    def _jit_forward(self, layer, inputs, paddings):
+        @jax.jit
+        def jit_forward(inputs, paddings):
+            return F(
+                layer,
+                inputs=dict(inputs=inputs, paddings=paddings),
+                is_training=True,
+                prng_key=jax.random.PRNGKey(123),
+                state={},
+            )
+
+        test_outputs, _ = jit_forward(inputs, paddings)
+        return test_outputs
+
+    @parameterized.product(
+        [
+            dict(frame_size_ms=25, hop_size_ms=10),
+            dict(frame_size_ms=25, hop_size_ms=25),
+        ],
+    )
+    @pytest.mark.fp64
+    def test_against_ref(self, frame_size_ms, hop_size_ms):
+        sample_rate, batch_size, max_seconds = 16_000, 4, 13
+        num_filters = 80
+
+        # Construct fake inputs.
+        inputs, paddings = _fake_audio(
+            batch_size=batch_size, seq_len=max_seconds * sample_rate, dtype=jnp.float64
+        )
+
+        # Compute ref outputs.
+        ref_outputs, ref_paddings = _ref_frontend(
+            inputs=inputs,
+            paddings=paddings,
+            sample_rate=sample_rate,
+            frame_size_ms=frame_size_ms,
+            hop_size_ms=hop_size_ms,
+            coeff=0.97,
+            num_filters=num_filters,
+            lower_edge_hertz=125.0,
+            upper_edge_hertz=7600.0,
+            mel_floor=1.0,
+        )
+
+        # Compute test outputs.
+        cfg: LogMelFrontend.Config = LogMelFrontend.default_config().set(
+            num_filters=num_filters,
+            sample_rate=sample_rate,
+            frame_size_ms=frame_size_ms,
+            hop_size_ms=hop_size_ms,
+        )
+        layer = cfg.set(name="test").instantiate(parent=None)
+        test_outputs = self._jit_forward(layer, inputs, paddings)
+
+        # Only compare the non-padding outputs.
+        ref_outputs = ref_outputs * (1 - tf.cast(ref_paddings, ref_outputs.dtype))[..., None]
+        self.assertAllClose(ref_outputs[..., None], test_outputs["outputs"])
+        self.assertAllClose(ref_paddings, test_outputs["paddings"])
+
+    @pytest.mark.fp64
+    def test_normalization(self):
+        sample_rate, batch_size, max_seconds = 16_000, 4, 13
+        num_filters, frame_size_ms, hop_size_ms = 80, 25, 10
+        cfg: LogMelFrontend.Config = LogMelFrontend.default_config().set(
+            num_filters=num_filters,
+            sample_rate=sample_rate,
+            frame_size_ms=frame_size_ms,
+            hop_size_ms=hop_size_ms,
+        )
+        layer = cfg.set(name="test").instantiate(parent=None)
+
+        def compute_mean_std(outputs):
+            # Compute mean/std of features. [batch, num_frames, num_filters, 1] -> [num_filters].
+            non_padding = jnp.sum(1 - outputs["paddings"])
+            mean = jnp.sum(outputs["outputs"], axis=(0, 1, -1)) / non_padding
+            mean_square = jnp.sum(outputs["outputs"] ** 2, axis=(0, 1, -1)) / non_padding
+            return mean, jnp.sqrt(mean_square - mean**2)
+
+        inputs, paddings = _fake_audio(
+            batch_size=batch_size, seq_len=max_seconds * sample_rate, dtype=jnp.float64
+        )
+        test_outputs = self._jit_forward(layer, inputs, paddings)
+
+        # Compute again with scaling by pre-computed mean and std.
+        mean, std = compute_mean_std(test_outputs)
+        cfg.output_transformation = config_for_function(
+            lambda: functools.partial(normalize_by_mean_std, mean=mean.tolist(), std=std.tolist())
+        )
+        layer = cfg.set(name="test").instantiate(parent=None)
+        test_outputs = self._jit_forward(layer, inputs, paddings)
+
+        # Check outputs.
+        mean, std = compute_mean_std(test_outputs)
+        self.assertTrue(jnp.allclose(mean, 0, atol=1e-5))
+        self.assertTrue(jnp.allclose(std, 1.0, atol=1e-5))
+
+
+def _ref_frontend(
+    *,
+    inputs: tf.Tensor,
+    paddings: tf.Tensor,
+    sample_rate: int,
+    frame_size_ms: int,
+    hop_size_ms: int,
+    coeff: float,
+    num_filters: int,
+    lower_edge_hertz: float,
+    upper_edge_hertz: float,
+    mel_floor: float,
+):
+    """Lingvo ASR frontend.
+
+    Reference:
+    https://github.com/tensorflow/lingvo/blob/4a9097a212622d99d7f8e2379804dbffdc44a97f/lingvo/tasks/asr/frontend.py#L330
+    """
+    frame_size = int(round(_ms_to_samples(frame_size_ms, sample_rate=sample_rate)))
+    frame_step = int(round(_ms_to_samples(hop_size_ms, sample_rate=sample_rate)))
+
+    inputs, output_paddings = _ref_framer(
+        inputs=inputs, paddings=paddings, frame_size=frame_size + 1, frame_step=frame_step
+    )
+    inputs = _ref_pre_emphasis(inputs=inputs, coeff=coeff)
+    inputs = tf.signal.hann_window(frame_size, dtype=inputs.dtype) * inputs
+    fft_size = int(max(512.0, math.pow(2, math.ceil(math.log(frame_size + 1, 2)))))
+    outputs = _ref_log_mel_spectrogram(
+        inputs=inputs,
+        fft_size=fft_size,
+        num_filters=num_filters,
+        sample_rate=sample_rate,
+        lower_edge_hertz=lower_edge_hertz,
+        upper_edge_hertz=upper_edge_hertz,
+        compute_energy=False,
+        mel_floor=mel_floor,
+    )
+    return outputs, output_paddings
