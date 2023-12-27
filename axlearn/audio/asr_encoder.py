2bde0f3999a8aa7849d8fc22a8dbac3bf256eb1a
zhiyun-lu <zhiyun_lu@apple.com>
2023-12-27 15:34:33 -0500

    make speech feature works with 3d input

--- axlearn/audio/asr_encoder.py
+++ axlearn/audio/asr_encoder.py
@@ -79,8 +79,12 @@ class SpeechFeatureLayer(BaseLayer):
         x = features["outputs"]
 
         if "augmenter" in self.children:
+            if len(features["outputs"].shape == 3):
+                x = x[..., None]
             # Apply augmentation.
             x = self.augmenter(inputs=x, paddings=features["paddings"])
+            if len(features["outputs"].shape == 3):
+                x = jnp.squeeze(x, axis=-1)
 
         # Apply subsampling.
         # [batch_size, subsampled_frames, subsampled_freq, output_dim].
