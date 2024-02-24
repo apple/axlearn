5f8c172f4c8a48f7e2f30058fd34edd72d495524
Ruoming Pang <r_pang@apple.com>
2024-02-24 10:16:54 -0500

    Adds `schedule_scale` summary to adastar_optimizer.

--- axlearn/common/optimizers.py
+++ axlearn/common/optimizers.py
@@ -1665,7 +1665,12 @@ def adastar_optimizer(
         def _update2(u: Tensor, param: OptParam):
             lr_scaled_updates = learning_rate * u
             updates_with_wd = lr_scaled_updates + weight_decay * param.value
-            return -update_schedule(step) * updates_with_wd
+            schedule_scale = update_schedule(step)
+            context = current_context()
+            if context:
+                context.add_summary("schedule_step", step)
+                context.add_summary("schedule_scale", schedule_scale)
+            return - schedule_scale * updates_with_wd
 
         updates2 = jax.tree_util.tree_map(lambda u, p: _update2(u, param=p), updates, params)
         return updates2, optax.safe_int32_increment(step)
