608e40a7933375ba9533fe201d0436d711af69f1
Patrick Toulme <ptoulme@amazon.com>
2024-04-22 21:25:14 +0000

    64 node working

--- run_trn.sh
+++ run_trn.sh
@@ -2,7 +2,6 @@
 #SBATCH --output=slurm-%x-%j.out
 #SBATCH --cpus-per-task 127
 #SBATCH --exclusive
-#SBATCH --nodes=8
-#SBATCH --exclude=compute1-st-kaena-training-0-[20,26,64-65,112,204,228,123,147,58,53,174,188,178,18]
+#SBATCH --nodes=64
 
 srun  --kill-on-bad-exit=1  run_trainer.sh
