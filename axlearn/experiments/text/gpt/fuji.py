153d48ecac3b95c0da4f0512a86a699a354fbc9b
Ruoming Pang <ruoming@gmail.com>
2024-04-29 20:31:51 -0400

    Adds versions V1,V2,V3 for the Fuji models. (#428)
    * Fixes single host configs.
    
    * Fixes single host configs.
    
    * Adds `zone` to {BaseBastionLaunchJob,SubmitBastionJob}.Config.
    
    * Makes c4_trainer.py use tpu-v5litepod-256x4 for fuji-7B.
    
    * Reverts changes to c4_trainer.py.
    
    * Reverts changes to c4_trainer.py.
    
    * Lint.
    
    * Fixes single host configs.
    
    * Adds versions V1,V2,V3 for the Fuji models.
    
    * Fixes eval seq length.
    
    * Fixes single host configs.
    
    * Fixes single host configs.
    
    * Fixes single host configs.
    
    * Fixes GQA and vocab size for the v3 models.
    
    * Fixes GQA and vocab size for the v3 models.
    
    * Fixes lint.
    
    * Adds a TODO to build the 128k vocab.
    
    * Adds step time for fuji-7B-v2-single-host.
    
    * Updates fuji v1 to be trained on 1T tokens.
    
    * Updates fuji v1 to be trained on 1T tokens.
    
    * Fixes RoPE theta for fuji v3.

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -6,12 +6,13 @@ Architecture names follow Apple varieties: fuji, gala, honeycrisp, etc.
 
 The fuji models are set up to imitate LLaMA-1 (https://arxiv.org/abs/2302.13971).
 """
-
+import enum
 from typing import Any, Dict, Optional, Union
 
 from axlearn.common import causal_lm, config
 from axlearn.common.attention import (
     CausalAttentionLogitBiasLayer,
+    FusedGroupedQKVLinear,
     FusedQKVLinear,
     RepeatedTransformerLayer,
     RoFormerQKVLinear,
@@ -23,11 +24,59 @@ from axlearn.experiments.text.gpt.common import model_config as common_model_con
 from axlearn.experiments.text.gpt.common import scaled_hidden_dim
 
 MODEL_SIZES = ("test", "7B")
-MAX_SEQUENCE_LENGTH = 2048
 
 
-def get_trainer_kwargs(model_size: str, *, vocab_size: int) -> Dict[str, Any]:
+class Version(enum.Enum):
+    V1 = 1
+    V2 = 2
+    V3 = 3
+
+
+# Mapping from Fuji versions to vocab sizes.
+VOCAB_SIZE = {
+    Version.V1: 32 * 1024,
+    Version.V2: 32 * 1024,
+    Version.V3: 128 * 1024,
+}
+
+
+# Mapping from Fuji versions to maximum sequence lengths.
+MAX_SEQUENCE_LENGTH = {
+    Version.V1: 2048,
+    Version.V2: 4096,
+    Version.V3: 8192,
+}
+
+
+ROPE_THETA = {
+    Version.V1: 1e4,
+    Version.V2: 1e4,
+    Version.V3: 5e5,
+}
+
+
+# Mapping from Fuji versions to total number of tokens used in training.
+TOTAL_TOKENS = {
+    Version.V1: 1 * (1024**4),  # 1T tokens
+    Version.V2: 2 * (1024**4),  # 2T tokens
+    Version.V3: 15 * (1024**4),  # 15T tokens
+}
+
+
+def get_trainer_kwargs(model_size: str, *, vocab_size: int, version: Version) -> Dict[str, Any]:
     """Construct default trainer kwargs given a model size."""
+    tokens_per_batch = 4 * (1024**2)  # 4M tokens.
+    max_step = TOTAL_TOKENS[version] // tokens_per_batch
+    max_sequence_length = MAX_SEQUENCE_LENGTH[version]
+    train_batch_size = tokens_per_batch // max_sequence_length
+
+    # Whether to use grouped query attention.
+    num_kv_heads = None
+    if version == Version.V3:
+        num_kv_heads = 8
+
+    rope_theta = ROPE_THETA[version]
+
     # dict() is more readable here.
     # pylint: disable=use-dict-literal
     if model_size == "test":
@@ -37,7 +86,9 @@ def get_trainer_kwargs(model_size: str, *, vocab_size: int) -> Dict[str, Any]:
                 hidden_dim=8,
                 ffn_dim=scaled_hidden_dim(scale=8 / 3, round_up_to_multiples_of=16),
                 num_heads=4,
+                num_kv_heads=2,
                 vocab_size=32,
+                rope_theta=rope_theta,
             ),
             learner_kwargs=dict(
                 peak_lr=6e-4,
@@ -54,10 +105,13 @@ def get_trainer_kwargs(model_size: str, *, vocab_size: int) -> Dict[str, Any]:
                 num_layers=32,
                 hidden_dim=128 * 32,
                 num_heads=32,
+                num_kv_heads=num_kv_heads,
+                rope_theta=rope_theta,
             ),
             learner_kwargs=dict(peak_lr=3e-4, weight_decay=0.1),
-            train_batch_size=4 * 1024 * 1024 // MAX_SEQUENCE_LENGTH,  # 4M tokens.
-            max_step=500_000,  # 2T tokens // 4M tokens/step.
+            max_sequence_length=max_sequence_length,
+            train_batch_size=train_batch_size,
+            max_step=max_step,
             mesh_shape=mesh_shape_from_axes(fsdp=-1),
             mesh_rules=(
                 # tpu-v4. step time: 3.03s.
@@ -65,6 +119,7 @@ def get_trainer_kwargs(model_size: str, *, vocab_size: int) -> Dict[str, Any]:
                 # tpu-v5e. step time: TBD.
                 ("tpu-v5litepod-256", mesh_shape_from_axes(data=-1, fsdp=16)),
                 # H100/A100 80G. Maximum per-node batch size = 64, hence need >= 32 nodes.
+                # p5.48xlarge 8x64. v1 step time: 1.54s.
                 (
                     "gpu-(p5.48xlarge|p4de.24xlarge)-(256|512|1024)",
                     mesh_shape_from_axes(data=-1, fsdp=8),
@@ -89,7 +144,9 @@ def model_config(
     num_layers: int,
     hidden_dim: int,
     num_heads: int,
+    num_kv_heads: Optional[int],
     vocab_size: int,
+    rope_theta: float,
     dropout_rate: float = 0.0,
     ffn_dim: Optional[Union[int, config.FunctionConfigBase]] = None,
 ) -> causal_lm.Model.Config:
@@ -99,7 +156,9 @@ def model_config(
         num_layers: The number of Transformer Layers.
         hidden_dim: The Transformer layer input/output dim.
         num_heads: The number of attention heads.
+        num_kv_heads: The optional number of KV heads. If not None, enables grouped query attention.
         vocab_size: The vocabulary size.
+        rope_theta: The theta value used for RoPE positional embeddings.
         dropout_rate: The dropout rate applied throughout the model.
             Defaults to 0.0 (i.e. no dropout).
         ffn_dim: The feed-forward dimension or config function.
@@ -112,6 +171,19 @@ def model_config(
     activation_fn = ("nn.silu", "linear")
     if ffn_dim is None:
         ffn_dim = scaled_hidden_dim(scale=8 / 3, round_up_to_multiples_of=256)
+    if num_kv_heads:
+        atten_input_linear = FusedGroupedQKVLinear.default_config().set(num_kv_heads=num_kv_heads)
+    else:
+        atten_input_linear = FusedQKVLinear.default_config()
+    atten_input_linear.cache_dtype = STEP_DTYPE
+    # RoPE embeddings: https://arxiv.org/abs/2104.09864.
+    atten_qkv_linear = RoFormerQKVLinear.default_config().set(
+        cache_dtype=STEP_DTYPE,
+        input_linear=atten_input_linear,
+        rotary_value=False,
+    )
+    atten_qkv_linear.rope_pos_emb_layer.theta = rope_theta
+
     cfg = common_model_config(
         num_layers=num_layers,
         hidden_dim=hidden_dim,
@@ -124,10 +196,6 @@ def model_config(
         dropout_rate=dropout_rate,
         emb_cfg=TransformerTextEmbeddings.default_config().set(pos_emb=None),
         attention_mask=CausalAttentionLogitBiasLayer.default_config(),
-        # RoPE embeddings: https://arxiv.org/abs/2104.09864.
-        attention_qkv_linear=RoFormerQKVLinear.default_config().set(
-            input_linear=FusedQKVLinear.default_config().set(cache_dtype=STEP_DTYPE),
-            rotary_value=False,
-        ),
+        attention_qkv_linear=atten_qkv_linear,
     )
     return cfg
