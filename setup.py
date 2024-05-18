1d44d48024a1d32c5b61092398acbcd51510b7ae
Ruoming Pang <r_pang@apple.com>
2024-05-18 15:44:40 +0000

    Simplifies setup.py.

--- setup.py
+++ setup.py
@@ -5,12 +5,8 @@ ext_modules = [
     Pybind11Extension(
         "profiler",
         ["profiler.cpp"],
-        extra_compile_args=['-g'],  # For debugging, remove in production
-        # extra_objects=['/usr/lib/x86_64-linux-gnu/libtcmalloc_and_profiler.a'],  # Static link
-        library_dirs=['/usr/lib/x86_64-linux-gnu'],  # Library directory
-        runtime_library_dirs=['/usr/lib/x86_64-linux-gnu'],  # Runtime library directory
-        include_dirs=['/usr/include'],  # Include directory, adjust as necessary
-        libraries=['tcmalloc_and_profiler'],  # Ensure the profiler library is linked
+        # Link tcmalloc_and_profiler to support heap profiler.
+        libraries=['tcmalloc_and_profiler'],
     ),
 ]
 
