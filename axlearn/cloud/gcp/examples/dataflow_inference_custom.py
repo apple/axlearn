631750a93bb35c34f0019fb9198b47745148bffb
Maggie Zhang <jiya.zhang.98@gmail.com>
2024-05-29 22:31:41 +0000

    Fix typing and missing returns

--- axlearn/cloud/gcp/examples/dataflow_inference_custom.py
+++ axlearn/cloud/gcp/examples/dataflow_inference_custom.py
@@ -8,7 +8,7 @@ Command line options:
 To debug locally:
 $ docker run -it --mount type=bind,src=$HOME/.config/gcloud,dst=/root/.config/gcloud \
     --entrypoint /bin/bash ${DOCKER_REPO}/${DOCKER_IMAGE}:{DOCKER_TAG}
-> python3 -m axlearn.cloud.gcp.dataflow_inference_custom \
+> python3 -m axlearn.cloud.gcp.examples.dataflow_inference_custom \
     --module=text.gpt.c4_trainer \
     --config=fuji-7B-single  \
     --trainer_dir='gs://.../checkpoints/step_xxx'
@@ -21,7 +21,7 @@ $ axlearn gcp dataflow start \
     --bundler_spec=target=dataflow \
     --bundler_spec=allow_dirty=True \
     --dataflow_spec=runner=DataflowRunner \
-    -- "'python3 -m axlearn.cloud.gcp.dataflow_inference_custom \
+    -- "'python3 -m axlearn.cloud.gcp.examples.dataflow_inference_custom \
         --module=text.gpt.c4_trainer \
         --config=fuji-7B-single \
         --trainer_dir='gs://.../checkpoints/step_xxx' \
@@ -37,7 +37,7 @@ $ axlearn gcp dataflow start \
     --dataflow_spec=runner=DataflowRunner \
     --dataflow_spec=dataflow_service_options=\
     "worker_accelerator=type:nvidia-l4;count:1;install-nvidia-driver" \
-    -- "'python3 -m axlearn.cloud.gcp.dataflow_inference_custom \
+    -- "'python3 -m axlearn.cloud.gcp.examples.dataflow_inference_custom \
         --module=text.gpt.c4_trainer \
         --config=fuji-7B-single \
         --trainer_dir='gs://.../checkpoints/step_xxx' \
@@ -66,11 +66,11 @@ from axlearn.common.utils import NestedTensor
 class CustomModelHandler(ModelHandler[Dict, PredictionResult, Any]):
     """Defines how to load a custom checkpoint and run inference."""
 
-    def __init__(self, flag_dict):
+    def __init__(self, flag_dict: Dict):
         # Store absl FLAGS in a flag dictionary to avoid pickling issues
         self._flag_dict = flag_dict
 
-    def _flag_values_from_dict(self, flag_values: dict) -> flags.FlagValues:
+    def _flag_values_from_dict(self, flag_values: Dict) -> flags.FlagValues:
         # Avoid mutating global FLAGS.
         fv = copy.deepcopy(flags.FLAGS)
         for k, v in flag_values.items():
@@ -95,7 +95,7 @@ class CustomModelHandler(ModelHandler[Dict, PredictionResult, Any]):
         # get InferenceRunner Config from Trainer Config and instantiate InferenceRunner
         inference_runner_cfg = InferenceRunner.config_from_trainer(trainer_config)
         inference_runner_cfg.init_state_builder.set(dir=flag_values.trainer_dir)
-        inference_runner = InferenceRunner(cfg=inference_runner_cfg, parent=None)
+        inference_runner = inference_runner_cfg.instantiate(parent=None)
 
         return inference_runner.create_method_runner(
             method="predict", prng_key=jax.random.PRNGKey(1)
@@ -106,7 +106,7 @@ class CustomModelHandler(ModelHandler[Dict, PredictionResult, Any]):
         batch: Sequence[NestedTensor],
         model: MethodRunner,
         inference_args: Optional[Dict[str, Any]] = None,
-    ):
+    ) -> Sequence[MethodRunner.Output]:
         """Runs inferences on a batch of NestedTensors.
         NestedTensor: https://github.com/apple/axlearn/blob/main/axlearn/common/utils.py#L56
 
@@ -129,7 +129,7 @@ class CustomModelHandler(ModelHandler[Dict, PredictionResult, Any]):
 class PostProcessFn(beam.DoFn):
     """Defines the transformations needed for post processing."""
 
-    def process(self, element):
+    def process(self, element: Any):
         logging.info(f"Inference finished. Output type: {type(element)}")
         yield None
 
