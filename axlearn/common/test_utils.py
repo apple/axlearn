23842c6946ac7fc4279f94cb47ec36fff6b6865e
apghml <143655008+apghml@users.noreply.github.com>
2024-06-02 19:50:13 +0000

    Call super().setUp() and super().tearDown() in test_utils.TestCase.

--- axlearn/common/test_utils.py
+++ axlearn/common/test_utils.py
@@ -134,6 +134,7 @@ class TestCase(parameterized.TestCase):
         return "FAKE"
 
     def setUp(self):
+        super().setUp()
         push_data_dir(self.data_dir)
         utils_spmd.setup(jax_backend=self._jax_backend())
 
@@ -142,6 +143,7 @@ class TestCase(parameterized.TestCase):
         return "cpu"
 
     def tearDown(self) -> None:
+        super().tearDown()
         self.assertEqual(pop_data_dir(), self.data_dir)
 
     def _compute_layer_outputs(
