695664f22564de7a7fa251060b40df70a069a666
apghml <143655008+apghml@users.noreply.github.com>
2024-06-05 13:45:28 -0700

    Add bundler size logging. (#509)

--- axlearn/cloud/common/bundler.py
+++ axlearn/cloud/common/bundler.py
@@ -48,6 +48,7 @@ import tempfile
 from typing import Dict, Iterable, List, Optional, Sequence, Type, TypeVar, Union
 from urllib.parse import urlparse
 
+import prefixed
 from absl import app, flags, logging
 from tensorflow import io as tf_io
 
@@ -195,6 +196,10 @@ class Bundler(Configurable):
             rel_config_file,
         )
         config.write_configs_with_header(str(rel_config_file), configs)
+
+        dir_size = sum(f.stat().st_size for f in temp_root.glob("**/*"))
+        dir_size = f"{prefixed.Float(dir_size):!.2k}B"
+        logging.info("Uncompressed size: %s", dir_size)
 
         return temp_dir
 
@@ -548,6 +553,8 @@ class BaseTarBundler(Bundler):
             with tarfile.open(tar_path, "w:gz") as tar:
                 for f in temp_root.glob("*"):
                     tar.add(f, arcname=f.name)
+            tar_size = f"{prefixed.Float(tar_path.stat().st_size):!.2k}B"
+            logging.info("Compressed size: %s", tar_size)
 
             # Upload to remote.
             remote_path = self.id(name)
