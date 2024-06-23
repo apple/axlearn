8323197b815b64d9e598591f2a64a8700fc1a8b5
Ruoming Pang <r_pang@apple.com>
2024-06-23 17:08:37 -0400

    Reverts changes to launch_trainer_main.py.

--- axlearn/common/launch_trainer_main.py
+++ axlearn/common/launch_trainer_main.py
@@ -2,16 +2,13 @@
 
 """Main function for launching the trainer."""
 
-import sys
-
-from absl import app, flags, logging
+from absl import app, flags
 
 from axlearn.common import launch, launch_trainer, measurement
 from axlearn.common.config import config_for_function
 
 
 def main(_):
-    logging.info("Command line: %s", " ".join(sys.argv))
     measurement.initialize(flags.FLAGS)
     launch.setup()
     trainer_config = launch_trainer.get_trainer_config()
