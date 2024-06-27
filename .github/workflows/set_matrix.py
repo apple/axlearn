30aec7ea0f2bb5cef5b3b4a6f0a9b973da34ca40
Sam Stoelinga <sammiestoel@gmail.com>
2024-06-26 23:51:29 -0700

    increase to 20 partitions

--- .github/workflows/set_matrix.py
+++ .github/workflows/set_matrix.py
@@ -1,13 +1,13 @@
 """
 A script to split up test files into partitions for GitHub Actions
 
-This allows us to run 10 partitions of tests at the same time on different
+This allows us to run 20 partitions of tests at the same time on different
 GitHub action runners.
 """
 import glob
 import json
 
-partitions = 10
+partitions = 20
 test_files = glob.glob("axlearn/**/*_test.py")
 # TODO(samos123): Figure out why clip is broken
 excluded_tests = ["axlearn/vision/clip_test.py"]
