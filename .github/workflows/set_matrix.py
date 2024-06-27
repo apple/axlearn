695d01b3623feb0afcef4d5e27fd99e47edc25fc
Sam Stoelinga <sammiestoel@gmail.com>
2024-06-26 19:12:49 -0700

    format code

--- .github/workflows/set_matrix.py
+++ .github/workflows/set_matrix.py
@@ -9,11 +9,11 @@ test_files = glob.glob("axlearn/**/*_test.py")
 # TODO(samos123): Figure out why clip is broken
 excluded_tests = ["axlearn/vision/clip_test.py"]
 test_files = [f for f in test_files if f not in excluded_tests]
-# Split the test files into partitions
+
+# Split the test files into partitions.
 paritioned_test_files: list[str] = [
     " ".join(test_files[i : i + partitions]) for i in range(0, len(test_files), partitions)
 ]
-
 
 print(
     json.dumps(
