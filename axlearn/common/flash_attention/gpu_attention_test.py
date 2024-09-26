ba8344668ab1326434aad0173671e5a892a4d2ec
Chang Lan <c.lan@apple.com>
2024-09-26 09:48:12 -0700

    Add tests for gpu_attention

--- axlearn/common/flash_attention/gpu_attention_test.py
+++ axlearn/common/flash_attention/gpu_attention_test.py
@@ -44,7 +44,8 @@ from axlearn.common.flash_attention.utils import mha_reference
 @pytest.mark.parametrize("use_fwd", [True, False])
 @pytest.mark.parametrize("causal", [True, False])
 @pytest.mark.parametrize("sm_scale", [1.0, 0.123])
-@pytest.mark.parametrize("bias_type", ["none", "matrix", "vector"])
+@pytest.mark.parametrize("use_bias", [True, False])
+@pytest.mark.parametrize("use_segment_ids", [True, False])
 @pytest.mark.skipif(jax.devices()[0].platform != "gpu", reason="Test only runs on GPU.")
 def test_fwd_against_ref(
     batch_size: int,
@@ -55,28 +56,33 @@ def test_fwd_against_ref(
     use_fwd: bool,
     causal: bool,
     sm_scale: float,
-    bias_type: str,
+    use_bias: bool,
+    use_segment_ids: bool,
 ):
     k1, k2, k3, k4 = jax.random.split(jax.random.PRNGKey(0), 4)
     q = jax.random.normal(k1, (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16)
     k = jax.random.normal(k2, (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16)
     v = jax.random.normal(k3, (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16)
 
-    if bias_type == "matrix":
-        bias = jax.random.normal(k4, (batch_size, num_heads, seq_len, seq_len), dtype=jnp.float16)
-    elif bias_type == "vector":
-        segment_left = jnp.ones((batch_size, seq_len // 2), dtype=jnp.int32)
-        segment_right = jnp.zeros((batch_size, seq_len // 2), dtype=jnp.int32)
-        bias = jnp.concatenate([segment_left, segment_right], axis=-1)
-    else:
-        bias = None
+    bias = (
+        jax.random.normal(k4, (batch_size, num_heads, seq_len, seq_len), dtype=jnp.float16)
+        if use_bias
+        else None
+    )
+
+    segment_left = jnp.ones((batch_size, seq_len // 2), dtype=jnp.int32)
+    segment_right = jnp.zeros((batch_size, seq_len // 2), dtype=jnp.int32)
+    segment_ids = (
+        jnp.concatenate([segment_left, segment_right], axis=-1) if use_segment_ids else None
+    )
+
     # Make sure that it is running on GPU.
     assert str(q.devices()) == "{cuda(id=0)}"
 
     if use_fwd:
 
         @jax.jit
-        def impl(q, k, v, bias):
+        def impl(q, k, v, bias, segment_ids):
             fn = functools.partial(
                 flash_attention,
                 block_q=block_size,
@@ -84,7 +90,7 @@ def test_fwd_against_ref(
                 causal=causal,
                 softmax_scale=sm_scale,
             )
-            out, _ = jax.vjp(fn, q, k, v, bias)
+            out, _ = jax.vjp(fn, q, k, v, bias, segment_ids)
             return out
 
     else:
@@ -96,8 +102,8 @@ def test_fwd_against_ref(
             softmax_scale=sm_scale,
         )
 
-    o = impl(q, k, v, bias)
-    o_ref = mha_reference(q, k, v, bias, causal=causal, softmax_scale=sm_scale)
+    o = impl(q, k, v, bias, segment_ids)
+    o_ref = mha_reference(q, k, v, bias, segment_ids, causal=causal, softmax_scale=sm_scale)
     chex.assert_trees_all_close(o, o_ref, atol=0.05)
 
 
@@ -112,7 +118,8 @@ def test_fwd_against_ref(
         (2, 8, 384, 64),
     ],
 )
-@pytest.mark.parametrize("bias_type", ["none", "matrix", "vector"])
+@pytest.mark.parametrize("use_bias", [True, False])
+@pytest.mark.parametrize("use_segment_ids", [True, False])
 @pytest.mark.parametrize("block_size", [128, 64])
 @pytest.mark.parametrize("causal", [True, False])
 @pytest.mark.skipif(jax.devices()[0].platform != "gpu", reason="Test only runs on GPU.")
@@ -121,7 +128,8 @@ def test_bwd_against_ref(
     num_heads: int,
     seq_len: int,
     per_head_dim: int,
-    bias_type: str,
+    use_bias: bool,
+    use_segment_ids: bool,
     block_size: int,
     causal: bool,
 ):
@@ -135,40 +143,47 @@ def test_bwd_against_ref(
         jax.random.PRNGKey(2), (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16
     )
 
-    if bias_type == "matrix":
-        bias = jax.random.normal(
+    bias = (
+        jax.random.normal(
             jax.random.PRNGKey(3), (batch_size, num_heads, seq_len, seq_len), dtype=jnp.float16
         )
-    elif bias_type == "vector":
-        segment_left = jnp.ones((batch_size, seq_len // 2), dtype=jnp.int32)
-        segment_right = jnp.zeros((batch_size, seq_len // 2), dtype=jnp.int32)
-        bias = jnp.concatenate([segment_left, segment_right], axis=-1)
-    else:
-        bias = None
+        if use_bias
+        else None
+    )
+
+    segment_left = jnp.ones((batch_size, seq_len // 2), dtype=jnp.int32)
+    segment_right = jnp.zeros((batch_size, seq_len // 2), dtype=jnp.int32)
+    segment_ids = (
+        jnp.concatenate([segment_left, segment_right], axis=-1) if use_segment_ids else None
+    )
+
     # Make sure that it is running on GPU.
     assert str(q.devices()) == "{cuda(id=0)}"
 
     sm_scale = q.shape[-1] ** -0.5
 
     # Compare outputs.
-    jax_out = flash_attention(q, k, v, bias, causal=causal, softmax_scale=sm_scale)
-    jax_ref_out = mha_reference(q, k, v, bias, causal=causal, softmax_scale=sm_scale)
+    jax_out = flash_attention(q, k, v, bias, segment_ids, causal=causal, softmax_scale=sm_scale)
+    jax_ref_out = mha_reference(q, k, v, bias, segment_ids, causal=causal, softmax_scale=sm_scale)
     chex.assert_trees_all_close(jax_out, jax_ref_out, atol=0.005)
 
-    def fn(q, k, v, bias):
+    def fn(q, k, v, bias, segment_ids):
         return flash_attention(
             q,
             k,
             v,
             bias,
+            segment_ids,
             causal=causal,
             softmax_scale=sm_scale,
             block_q=block_size,
             block_k=block_size,
         ).sum()
 
-    def ref_fn(q, k, v, bias):
-        return mha_reference(q, k, v, bias, causal=causal, softmax_scale=sm_scale).sum()
+    def ref_fn(q, k, v, bias, segment_ids):
+        return mha_reference(
+            q, k, v, bias, segment_ids, causal=causal, softmax_scale=sm_scale
+        ).sum()
 
     # Compare gradients.
     jax_grads = jax.grad(fn, argnums=(0, 1, 2))(q, k, v, bias)
