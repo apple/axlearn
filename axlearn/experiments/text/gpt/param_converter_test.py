d14a734212a9c6a0538145a51d98d5c27038db11
sychen52 <sychen52@gmail.com>
2024-11-19 17:22:31 +0000

    Add llama 3 tokenizer
    add a new version called V3_TIKTOKEN.
    
    other edits based on suggestions.

--- axlearn/experiments/text/gpt/param_converter_test.py
+++ axlearn/experiments/text/gpt/param_converter_test.py
@@ -9,7 +9,7 @@ import numpy as np
 import pytest
 import torch
 from absl.testing import absltest, parameterized
-from transformers import AutoConfig
+from transformers.models.llama.configuration_llama import LlamaConfig
 from transformers.models.llama.modeling_llama import LlamaForCausalLM
 
 from axlearn.common import utils
@@ -26,6 +26,69 @@ from axlearn.experiments.text.gpt import c4_trainer
 dir_path = os.path.dirname(os.path.realpath(__file__))
 # Use cpu for the test.
 jax.config.update("jax_platform_name", "cpu")
+
+# Parameters are based on https://huggingface.co/meta-llama/Llama-3.2-1B/blob/main/config.json
+config_dict_1b = {
+    "vocab_size": 128256,
+    "hidden_size": 2048,
+    "intermediate_size": 8192,
+    "num_hidden_layers": 16,
+    "num_attention_heads": 32,
+    "num_key_value_heads": 8,
+    "hidden_act": "silu",
+    "max_position_embeddings": 131072,
+    "initializer_range": 0.02,
+    "rms_norm_eps": 1e-5,
+    "use_cache": True,
+    "bos_token_id": 128000,
+    "eos_token_id": 128001,
+    "pretraining_tp": 1,
+    "tie_word_embeddings": True,
+    "rope_theta": 500000.0,
+    "rope_scaling": {
+        "factor": 32.0,
+        "high_freq_factor": 4.0,
+        "low_freq_factor": 1.0,
+        "original_max_position_embeddings": 8192,
+        "rope_type": "llama3",
+    },
+    "attention_bias": False,
+    "attention_dropout": 0.0,
+    "mlp_bias": False,
+    "torch_dtype": "bfloat16",
+    "architectures": ["LlamaForCausalLM"],
+}
+# Parameters are based on https://huggingface.co/meta-llama/Llama-3.2-3B/blob/main/config.json
+config_dict_3b = {"hidden_size": 3072, "num_attention_heads": 24, "num_hidden_layers": 28}
+# Parameters are based on https://huggingface.co/meta-llama/Llama-3.1-8B/blob/main/config.json
+config_dict_8b = {
+    "hidden_size": 4096,
+    "intermediate_size": 14336,
+    "num_hidden_layers": 32,
+    "rope_scaling": {
+        "factor": 8.0,
+        "high_freq_factor": 4.0,
+        "low_freq_factor": 1.0,
+        "original_max_position_embeddings": 8192,
+        "rope_type": "llama3",
+    },
+    "tie_word_embeddings": False,
+}
+# Parameters are based on https://huggingface.co/meta-llama/Llama-3.1-70B/blob/main/config.json
+config_dict_70b = {
+    "hidden_size": 8192,
+    "intermediate_size": 28672,
+    "num_attention_heads": 64,
+    "num_hidden_layers": 80,
+    "rope_scaling": {
+        "factor": 8.0,
+        "high_freq_factor": 4.0,
+        "low_freq_factor": 1.0,
+        "original_max_position_embeddings": 8192,
+        "rope_type": "llama3",
+    },
+    "tie_word_embeddings": False,
+}
 
 
 def compute_fuji_grad(prng_key, fuji: Model, state: NestedTensor, input_batch: NestedTensor):
@@ -76,25 +139,13 @@ def compute_llama_grad(llama, torch_ids, state):
 
 class FujiConvertStateTest(TestCase):
     @parameterized.parameters(
-        dict(
-            fuji_model_name="fuji-1B-v3",
-            llama_model_name="Llama-3.2-1B",
-        ),
-        dict(
-            fuji_model_name="fuji-3B-v3",
-            llama_model_name="Llama-3.2-3B",
-        ),
-        dict(
-            fuji_model_name="fuji-8B-v3",
-            llama_model_name="Llama-3.1-8B",
-        ),
-        dict(
-            fuji_model_name="fuji-70B-v3",
-            llama_model_name="Llama-3.1-70B",
-        ),
+        dict(fuji_model_name="fuji-1B-v3-tiktoken"),
+        dict(fuji_model_name="fuji-3B-v3-tiktoken"),
+        dict(fuji_model_name="fuji-8B-v3-tiktoken"),
+        dict(fuji_model_name="fuji-70B-v3-tiktoken"),
     )
     @pytest.mark.high_cpu
-    def test_weight_loading(self, fuji_model_name, llama_model_name):
+    def test_weight_loading(self, fuji_model_name):
         trainer_config_map = c4_trainer.named_trainer_configs()
         trainer_config_fn = trainer_config_map[fuji_model_name]
         trainer_config = trainer_config_fn()
@@ -103,17 +154,14 @@ class FujiConvertStateTest(TestCase):
         fuji: Model = model_config.instantiate(parent=None)
         prng_key = jax.random.PRNGKey(0)
         state = fuji.initialize_parameters_recursively(prng_key=prng_key)
-        config = AutoConfig.from_pretrained(
-            os.path.join(
-                dir_path,
-                "..",
-                "..",
-                "testdata",
-                "axlearn.experiments.text.gpt.param_converter_test",
-                f"{llama_model_name}.json",
-            ),
-            local_files_only=True,
-        )
+        config_dict = config_dict_1b
+        if fuji_model_name == "fuji-3B-v3-tiktoken":
+            config_dict.update(config_dict_3b)
+        elif fuji_model_name == "fuji-8B-v3-tiktoken":
+            config_dict.update(config_dict_8b)
+        elif fuji_model_name == "fuji-70B-v3-tiktoken":
+            config_dict.update(config_dict_70b)
+        config = LlamaConfig(**config_dict)
         llama = LlamaForCausalLM._from_config(config)  # pylint: disable=W0212
         llama = llama.eval()
         ids = jax.random.randint(jax.random.PRNGKey(123), shape=(2, 2), minval=0, maxval=128256)
@@ -135,13 +183,13 @@ class FujiConvertStateTest(TestCase):
         llama_logits = output.logits.numpy()
 
         # The difference is caused by the SDPA attention layer. The deeper the larger the error.
-        if fuji_model_name == "fuji-1B-v3":
+        if fuji_model_name == "fuji-1B-v3-tiktoken":
             atol = 2e-3
-        elif fuji_model_name == "fuji-3B-v3":
+        elif fuji_model_name == "fuji-3B-v3-tiktoken":
             atol = 2e-2
-        elif fuji_model_name == "fuji-8B-v3":
+        elif fuji_model_name == "fuji-8B-v3-tiktoken":
             atol = 2e-1
-        elif fuji_model_name == "fuji-70B-v3":
+        elif fuji_model_name == "fuji-70B-v3-tiktoken":
             atol = 2.0
         else:
             atol = 2e-3
---
+++ axlearn/experiments/text/gpt/vocabulary_fuji_v3.py
@@ -0,0 +1,201 @@
+# Copyright © 2024 Apple Inc.
+
+"""Fuji v3 vocabulary."""
+
+import os
+import tempfile
+from typing import Optional, Protocol, Sequence, Union
+
+import jax
+import numpy as np
+import tensorflow.compat.v2 as tf
+from tokenizers import Tokenizer
+
+import axlearn.common.file_system as fs
+from axlearn.common.utils import get_data_dir
+
+
+class InnerTokenizer(Protocol):
+    """Defines a protocol of InnerTokenizer which is used in Vocabulary.
+
+    This is a subset of sentencepiece_processor.SentencePieceProcessor API that are used in
+    Vocabulary.
+    """
+
+    def encode_as_pieces(self, pieces: str) -> list[str]:
+        """Encode text input to tokens."""
+        pass
+
+    def piece_to_id(self, piece: str) -> int:
+        """Encode a token to id."""
+        pass
+
+
+class Vocabulary(Protocol):
+    """Defines a protocol of Vocabulary.
+
+    This is a subset of seqio.Vocabulary APIs that are used in text_to_lm_training_input and
+    test_to_lm_eval_input.
+    """
+
+    @property
+    def pad_id(self) -> int:
+        pass
+
+    @property
+    def eos_id(self) -> Optional[int]:
+        pass
+
+    def encode_tf(self, s: tf.Tensor) -> tf.Tensor:
+        """Tokenizes string Scalar to an int32 Tensor, without adding EOS."""
+        pass
+
+    def _decode_tf(self, ids: tf.Tensor) -> tf.Tensor:
+        """Detokenizes int32 batched Tensor."""
+        pass
+
+    def encode(self, s: str) -> list[int]:
+        """Tokenizes string to an int sequence, without adding EOS."""
+        pass
+
+    def _decode(self, ids: Sequence[int]) -> str:
+        """Detokenizes int sequence to a string, through all EOS."""
+        pass
+
+    def decode(self, ids: Sequence[int]) -> str:
+        """Detokenizes int32 iterable to a string, up through first EOS."""
+        pass
+
+    @property
+    def tokenizer(self) -> InnerTokenizer:
+        pass
+
+
+class FujiInnerTokenizer:
+    """A wrapper for tokenizer.Tokenizer so that it follows InnerTokenizer Protocol."""
+
+    def __init__(self, tokenizer):
+        self._tokenizer = tokenizer
+
+    def encode_as_pieces(self, pieces: str) -> list[str]:
+        """Encode text input to tokens."""
+        return self._tokenizer.encode(pieces, add_special_tokens=False).tokens
+
+    def piece_to_id(self, piece: str) -> int:
+        """Encode a token to id."""
+        return self._tokenizer.token_to_id(piece)
+
+
+class FujiV3Vocabulary:
+    """A wrapper for tokenizers.Tokenizer so that it follows Vocabulary Protocol.
+
+    Although its name has fuji, but it can be extended to work for all tokenizers.Tokenizer.
+    """
+
+    def __init__(self, filename: str):
+        data_dir = get_data_dir()
+        data_dir = (
+            os.path.join(os.path.dirname(__file__), "..", "..", "..", "data")
+            if data_dir is None or data_dir == "FAKE"
+            else data_dir
+        )
+        filename = os.path.join(data_dir, "tokenizers", "hf", filename)
+        if filename.startswith("gs:") or filename.startswith("s3:"):
+            # Create a different file for each usage.
+            with tempfile.TemporaryDirectory() as tmpdir:
+                path = os.path.join(tmpdir, "tokenizer.json")
+                fs.copy(filename, path)
+                self._tokenizer = Tokenizer.from_file(path)
+        else:
+            self._tokenizer = Tokenizer.from_file(filename)
+        self.vocab = self._tokenizer.get_vocab()
+        self.tokenizer = FujiInnerTokenizer(self._tokenizer)
+
+    @property
+    def pad_id(self) -> int:
+        # Some tokenizers do not have a pad_id.
+        # https://discuss.huggingface.co/t/how-to-set-the-pad-token-for-meta-llama-llama-3-models/103418
+        for token in ("<|pad_id|>", "<|finetune_right_pad_id|>"):
+            if token in self.vocab:
+                return self.vocab[token]
+        else:
+            raise ValueError("Unable to infer pad token.")
+
+    @property
+    def eos_id(self) -> Optional[int]:
+        if "<|end_of_text|>" in self.vocab:
+            return self.vocab["<|end_of_text|>"]
+        else:
+            raise NotImplementedError()
+
+    def _encode_tf(self, s: tf.Tensor) -> tf.Tensor:
+        """Encodes a string to token IDs.
+
+        Args:
+            s: A tf.Tensor of shape () or (n,) and dtype tf.string.
+
+        Returns:
+            A tf.Tensor or RaggedTensor of shape (num_tokens,) or (n, None) and dtype tf.int32.
+        """
+        need_unpack = False
+        if s.ndim == 0:
+            s = tf.reshape(s, (1,))
+            need_unpack = True
+
+        def helper(s):
+            s = s.numpy()
+            res = self._tokenizer.encode_batch([item.decode("utf-8") for item in s])
+            return tf.ragged.constant([r.ids for r in res])
+
+        ret = tf.py_function(
+            helper, inp=[s], Tout=tf.RaggedTensorSpec([None, None], dtype=tf.int32)
+        )
+        if need_unpack:
+            return ret[0]
+        else:
+            return ret
+
+    def _decode_tf(self, ids: tf.Tensor) -> tf.Tensor:
+        """Detokenizes int32 batched Tensor."""
+        need_unpack = False
+        if ids.ndim == 1:
+            ids = tf.reshape(ids, (1, -1))
+            need_unpack = True
+
+        def helper(ids):
+            s = self._tokenizer.decode_batch(ids.numpy().tolist(), skip_special_tokens=True)
+            return tf.convert_to_tensor(s, dtype=tf.string)
+
+        ret = tf.py_function(helper, inp=[ids], Tout=tf.string)
+        ret.set_shape(tf.TensorShape((None,)))
+        if need_unpack:
+            return ret[0]
+        else:
+            return ret
+
+    def encode_tf(self, s: tf.Tensor) -> tf.Tensor:
+        """Tokenizes string Scalar to an int32 Tensor, without adding EOS.
+
+        Args:
+            s: A tf.Tensor of shape () or (n,) and dtype tf.string.
+
+        Returns:
+            A tf.Tensor or RaggedTensor of shape (num_tokens,) or (n, None) and dtype tf.int32.
+        """
+        return self._encode_tf(s)
+
+    def encode(self, s: str) -> list[int]:
+        """Tokenizes string to an int sequence, without adding EOS."""
+        return self._tokenizer.encode(s).ids
+
+    def _decode(self, ids: Union[list[int], tuple[int]]) -> str:
+        """Detokenizes int32 iterable to a string."""
+        return self._tokenizer.decode(ids)
+
+    def decode(self, ids: Union[list[int], tuple[int], jax.Array, np.ndarray]) -> str:
+        """Detokenizes int32 iterable to a string, up through first EOS."""
+        if self.eos_id is not None and self.eos_id in ids:
+            if isinstance(ids, (jax.Array, np.ndarray)):
+                ids = ids.tolist()  # type: ignore
+            ids = ids[: ids.index(self.eos_id) + 1]
+        return self._decode(ids)
