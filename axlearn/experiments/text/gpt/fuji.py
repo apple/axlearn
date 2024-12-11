041a54e5f5d6cebfd7b5ae4c4b87666ad2286638
Apoorv Gupta <apoorvgu@amazon.com>
2024-12-11 20:50:22 +0000

    revert train_batch size change

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -415,7 +415,7 @@ def get_trainer_kwargs(
             ),
             learner_kwargs=dict(peak_lr=1.5e-4, weight_decay=0.1),
             max_sequence_length=max_sequence_length,
-            train_batch_size=int((jax.device_count()/4)),
+            train_batch_size=train_batch_size,
             input_partition_type=None if backend != "neuron" else DataPartitionType.BATCH,
             max_step=max_step,
             mesh_shape=mesh_shape_from_axes(fsdp=-1),
