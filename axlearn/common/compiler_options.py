11b9ea74596189692dbabfca120f79563dbcb6a7
Ethan Li <ethanli@apple.com>
2024-12-12 13:55:27 -0800

    Fix v6e boolean flags

--- axlearn/common/compiler_options.py
+++ axlearn/common/compiler_options.py
@@ -53,28 +53,28 @@ def default_xla_options(
             megascale_grpc_premap_memory_bytes=17179869184,
             # Improved performance for v6e.
             xla_tpu_scoped_vmem_limit_kib=98304,
-            xla_tpu_enable_async_collective_fusion=True,
-            xla_tpu_enable_async_collective_fusion_fuse_all_gather=True,
-            xla_tpu_enable_async_collective_fusion_multiple_steps=True,
-            xla_tpu_overlap_compute_collective_tc=True,
-            xla_enable_async_all_gather=True,
+            xla_tpu_enable_async_collective_fusion="true",
+            xla_tpu_enable_async_collective_fusion_fuse_all_gather="true",
+            xla_tpu_enable_async_collective_fusion_multiple_steps="true",
+            xla_tpu_overlap_compute_collective_tc="true",
+            xla_enable_async_all_gather="true",
             # Host offloading flags
-            xla_tpu_enable_all_experimental_scheduler_features=True,
+            xla_tpu_enable_all_experimental_scheduler_features="true",
             # Flag to enable memory tracking scheduling. The default AUTO only enables
             # it in some situations. Not needed if
             # xla_tpu_enable_all_experimental_scheduler_features is set to true already.
-            xla_tpu_enable_scheduler_memory_pressure_tracking=True,
+            xla_tpu_enable_scheduler_memory_pressure_tracking="true",
             # Flag controlling the maximum number of overlapping host offloadings.
             xla_tpu_host_transfer_overlap_limit=24,
             # Flag to enable the aggressive removal of opt-barriers.
-            xla_tpu_aggressive_opt_barrier_removal=True,
+            xla_tpu_aggressive_opt_barrier_removal="true",
             # Flag to enable more aggressive scheduling for async ops, such as pushing
             # the async start to the beginning of the loop body.
-            xla_lhs_prioritize_async_depth_over_stall=True,
+            xla_lhs_prioritize_async_depth_over_stall="true",
             # Flag to enable pipelining of cross-DCN all-gathers.
-            xla_tpu_enable_ag_backward_pipelining=True,
-            xla_should_allow_loop_variant_parameter_in_chain=True,
-            xla_should_add_loop_invariant_op_in_chain=True,
+            xla_tpu_enable_ag_backward_pipelining="true",
+            xla_should_allow_loop_variant_parameter_in_chain="true",
+            xla_should_add_loop_invariant_op_in_chain="true",
             # Flag controlling the maximum number of overlapping cross-DCN send/recv.
             xla_max_concurrent_host_send_recv=100,
             # Flag controlling the HBM memory limit as a percentage of the total HBM size.
@@ -92,16 +92,16 @@ def default_xla_options(
             # though, may make the scheduler weighting too much on the memory usage instead
             # of latency side.
             xla_latency_hiding_scheduler_rerun=2,
-            xla_tpu_use_enhanced_launch_barrier=True,
+            xla_tpu_use_enhanced_launch_barrier="true",
             # Sparsecore offloading for all reduce.
             # Uncomment below flags to enable it.
-            # xla_sc_disable_megacore_partitioning=True,
-            # xla_tpu_use_tc_device_shape_on_sc=True,
-            # tpu_use_continuations=True,
+            # xla_sc_disable_megacore_partitioning="true",
+            # xla_tpu_use_tc_device_shape_on_sc="true",
+            # tpu_use_continuations="true",
             # xla_jf_crs_combiner_threshold_count=10,
             # xla_sc_enable_instruction_fusion="false",
             # xla_sc_disjoint_spmem="false",
-            # xla_tpu_enable_sparse_core_collective_offload_all_reduce=True,
+            # xla_tpu_enable_sparse_core_collective_offload_all_reduce="true",
         )
         # This flag can be removed after upgrading to Jax 0.4.38.
         # Uncomment for sparsecore offloading.
