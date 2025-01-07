4a434c85d0101b7f96b538bf089bcf85a0a12002
Hanzhi Zhou <hanzhi_zhou@apple.com>
2025-01-07 14:28:36 -0800

    Fix name

--- axlearn/common/flash_attention/conftest.py
+++ axlearn/common/flash_attention/conftest.py
@@ -7,7 +7,8 @@ Usage on 8 GPU machines: PARALLEL_GPU_TEST=1 pytest -n 8 gpu_attention_test.py
 import os
 
 
-def pytest_configure(_):
+# pylint: disable-next=unused-argument
+def pytest_configure(config):
     if "PARALLEL_GPU_TEST" not in os.environ:
         return
     worker_id = os.getenv("PYTEST_XDIST_WORKER", "gw0")
