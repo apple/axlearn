48bf488c10f0d25bd6236759a5f4ebc8f2f5355f
Haoshuo Huang <haoshuo_huang@apple.com>
2025-02-10 22:25:18 -0800

    Add support for grain.IterDataset in sampling (#980)

--- axlearn/common/input_grain_test.py
+++ axlearn/common/input_grain_test.py
@@ -85,37 +85,65 @@ class UtilsTest(TestCase):
         dict(
             sources=[slice(0, 10, 2), slice(1, 5, 2)],
             weights=[1, 1],
+            is_iter_dataset=[False, False],
             take=10,
             expected=[0, 1, 2, 3, 4, 1, 6, 3, 8, 1],
         ),
         dict(
             sources=[slice(0, 10, 2), slice(1, 5, 2)],
             weights=[2, 1],
+            is_iter_dataset=[False, False],
             take=10,
             expected=[0, 2, 1, 4, 6, 3, 8, 0, 2, 1],
         ),
         dict(
             sources=[slice(0, 10, 2), slice(1, 5, 2)],
             weights=[1, 1e-9],
+            is_iter_dataset=[False, False],
             take=10,
             expected=[0, 2, 4, 6, 8, 0, 2, 4, 6, 8],
+        ),
+        # IterDataset
+        dict(
+            sources=[slice(0, 10, 2), slice(1, 5, 2)],
+            weights=[1, 1],
+            is_iter_dataset=[True, True],
+            take=10,
+            expected=[0, 1, 2, 3, 4, 1, 6, 3, 8, 1],
+        ),
+        # Mixture of IterDataset and MapDataset.
+        dict(
+            sources=[slice(0, 10, 2), slice(1, 5, 2)],
+            weights=[1, 1],
+            is_iter_dataset=[True, False],
+            take=10,
+            expected=[0, 1, 2, 3, 4, 1, 6, 3, 8, 1],
         ),
     )
     def test_sample_from_datasets(
         self,
         sources: list[slice],
         weights: list[int],
+        is_iter_dataset: list[bool],
         take: Optional[int],
         expected: list[int],
     ):
+        sources = [
+            range_dataset(start=src.start, stop=src.stop, step=src.step).repeat() for src in sources
+        ]
+        sources = [
+            source.to_iter_dataset() if should_convert else source
+            for source, should_convert in zip(sources, is_iter_dataset)
+        ]
         ds = sample_from_datasets(
-            sources=[
-                range_dataset(start=src.start, stop=src.stop, step=src.step) for src in sources
-            ],
+            sources=sources,
             weights=weights,
         )
-        ds = ds.slice(slice(0, take))
-        self.assertCountEqual(expected, list(ds))
+        ds_iter = iter(ds)
+        result = []
+        for _ in range(take):
+            result.append(next(ds_iter))
+        self.assertCountEqual(expected, list(result))
 
     def test_sample_from_datasets_errors(self):
         ds = range_dataset(start=0, stop=2)
@@ -124,17 +152,12 @@ class UtilsTest(TestCase):
         repeated_ds = sample_from_datasets(sources=[ds], weights=[1]).slice(slice(0, 4))
         self.assertEqual([0, 1, 0, 1], list(repeated_ds))
 
-        # Make sure that non-map dataset raises.
-        with self.assertRaisesRegex(ValueError, "MapDataset"):
-            ds = ds.to_iter_dataset()
-            sample_from_datasets(sources=[ds], weights=[1])
-
     def test_shuffle_dataset(self):
         # Test without repeat.
         ds = sample_from_datasets(
             sources=[
-                range_dataset(start=0, stop=10, step=2),
-                range_dataset(start=1, stop=5, step=2),
+                range_dataset(start=0, stop=10, step=2).repeat(),
+                range_dataset(start=1, stop=5, step=2).repeat(),
             ],
             weights=[2, 1],
         )
@@ -174,7 +197,7 @@ class UtilsTest(TestCase):
 
     def test_batch(self):
         # [0, 1, 2, 3, 4].
-        ds = range_dataset(start=0, stop=5, seed=123)
+        ds = range_dataset(start=0, stop=5, seed=123).repeat()
         # [1, 2, 3, 4, 5].
         other_ds = ds.map(_PlusOne())
         # [0, 1, 2, 1, 3, 4, 2, 5, 1, 3, ...].
