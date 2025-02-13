31e8da0c278a96d72dc1d82411205bab50392a39
Alexander Pivovarov <apivovarov@gmail.com>
2025-02-13 15:44:51 -0800

    Fix Missing return statement in base_layer_test.py::ExplicitFanLayer::_compute_fan_axes (#987)

--- axlearn/common/base_layer_test.py
+++ axlearn/common/base_layer_test.py
@@ -614,7 +614,7 @@ class ComputeFanAxesTest(TestCase):
         def _compute_fan_axes(self, name: str, parameter_spec: ParameterSpec) -> Optional[FanAxes]:
             if name == "fan_axes_specified_weight":
                 raise RuntimeError("Should not be invoked.")
-            super()._compute_fan_axes(name, parameter_spec)
+            return super()._compute_fan_axes(name, parameter_spec)
 
     @parameterized.parameters(
         (
