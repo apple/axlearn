d207e577946735bc975940206693bb8c1e4030f6
Hanzhi Zhou <hanzhi_zhou@apple.com>
2025-02-26 17:24:35 -0800

    Rename

--- axlearn/common/utils_test.py
+++ axlearn/common/utils_test.py
@@ -911,9 +911,9 @@ class TreeUtilsTest(TestCase):
 
         with self.assertRaises(ValueError):
             # c is not a VDict.
-            tree_merge(primary, secondary={"a": {"c": {"e": 456}}}, override_fn=lambda f, s: s)
+            tree_merge(primary, secondary={"a": {"c": {"e": 456}}}, leaf_merge_fn=lambda f, s: s)
         out = tree_merge(
-            primary, secondary={"a": {"c": VDict({"e": 456})}}, override_fn=lambda f, s: s
+            primary, secondary={"a": {"c": VDict({"e": 456})}}, leaf_merge_fn=lambda f, s: s
         )
         self.assertEqual(
             out, {"a": {"b": {}, "c": VDict({"e": 456}), "g": {"e": 123}}, "empty": ()}
