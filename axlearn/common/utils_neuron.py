354e3a70fc0ad16a0d03a7e3c900c796692f669e
aahila-aws <aahila@amazon.com>
2025-04-17 22:14:02 +0000

    minor mishap

--- axlearn/common/utils_neuron.py
+++ axlearn/common/utils_neuron.py
@@ -30,11 +30,8 @@ from axlearn.common.layers import (
 jax.config.update('jax_platform_name', 'cpu')
 from axlearn.common.utils import PartitionSpec, infer_mesh_shape, cast_floats
 from axlearn.experiments.text.gpt.common import MESH_AXIS_NAMES, mesh_shape_from_axes
-<<<<<<< HEAD
 from axlearn.common.param_init import PARAM_REGEXP_WEIGHT, DefaultInitializer, WeightInitializer
-=======
 from axlearn.experiments.text.gpt.envy import MOE_OUTER_BATCH_AXIS_NAMES, MOE_DIM_TO_MESH_AXIS_MAP
->>>>>>> origin/aws-moe
 
 # FP32 test tolerances
 TEST_TOLS_FP32 = {
@@ -47,30 +44,6 @@ TEST_TOLS_BF16 = {
     "rtol": 1e-2,
 }
 
-<<<<<<< HEAD
-MOE_OUTER_BATCH_AXIS_NAMES = ("data", "fsdp")
-
-MOE_DIM_TO_MESH_AXIS_MAP = {
-    "me": PartitionSpec(None, None),
-    "emh": PartitionSpec("expert", "fsdp", "model"),
-    "ehm": PartitionSpec("expert", "model", "fsdp"),
-    "ehM": PartitionSpec("expert", "model", None),
-    "ogsm": PartitionSpec(MOE_OUTER_BATCH_AXIS_NAMES, "expert", None, "model"),
-    "ogsM": PartitionSpec(MOE_OUTER_BATCH_AXIS_NAMES, "expert", None, None),
-    "ogse": PartitionSpec(MOE_OUTER_BATCH_AXIS_NAMES, "expert", None, None),
-    "ogec": PartitionSpec(MOE_OUTER_BATCH_AXIS_NAMES, "expert", None, None),
-    # Dispatch and combine tensors.
-    "ogsec": PartitionSpec(MOE_OUTER_BATCH_AXIS_NAMES, None, None, "expert", None),
-    "oegcm": PartitionSpec(MOE_OUTER_BATCH_AXIS_NAMES, "expert", None, None, "model"),
-    "oegcM": PartitionSpec(MOE_OUTER_BATCH_AXIS_NAMES, "expert", None, None, None),
-    "ogecm": PartitionSpec(MOE_OUTER_BATCH_AXIS_NAMES, None, "expert", None, "model"),
-    "ogecM": PartitionSpec(MOE_OUTER_BATCH_AXIS_NAMES, None, "expert", None, None),
-    "oegch": PartitionSpec(MOE_OUTER_BATCH_AXIS_NAMES, "expert", None, None, "model"),
-    "hoesm": PartitionSpec("model", MOE_OUTER_BATCH_AXIS_NAMES, "expert", None, None),
-}
-
-=======
->>>>>>> origin/aws-moe
 class ModuleConfig():
     def __init__(self, module = None, device = "cpu", layer = None, dtype = jnp.float32):
         assert module is not None
