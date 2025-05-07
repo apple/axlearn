fb474c34ea4f4809d5296474df0f4b7957e4dd7f
Matthew Hopkins <matthew_e_hopkins@apple.com>
2025-05-06 22:16:39 -0700

    Pre 0.5.3 changes (#1164)
    * add set_threefry_partitionable decorator for tests
    
    Jax 0.5.0 will enable threefry partitionable by default. Add a decorator to
    easily and temporarily mark tests that use static data from rng for future
    updating.
    
    * apply set_threefry_partitionable to tests that will be affected by jax 0.5.0
    
    * update NamedSharding usages
    
    in checkpointer and utils if mesh_axes is None use PartitionSpec() for
    future jax 0.5.0 support
    
    * remove enable_memories config usages
    
    enable_memories was changed to default True in jax 0.4.32 and the flag
    will be removed in jax 0.5.0
    
    * update BaseLayerTest::test_tensor_stats for threefry_partitionable
    
    jax 0.5.0 will default enable threefry_partitionable, update this tests
    comparison values.
    
    * Update axlearn/common/evaler_test.py
    
    add explanatory comment
    
    Co-authored-by: Kelvin Zou <xuan_zou@apple.com>
    
    * Update axlearn/common/test_utils.py
    
    additional comment for set_threefry_partionable
    
    Co-authored-by: Kelvin Zou <xuan_zou@apple.com>
    
    * update attention_test
    
    Prepare tests for jax 0.5.0
    
    * linting fix
    
    ---------
    
    Co-authored-by: Kelvin Zou <xuan_zou@apple.com>

--- axlearn/common/base_layer_test.py
+++ axlearn/common/base_layer_test.py
@@ -41,7 +41,7 @@ from axlearn.common.param_init import (
     FanAxes,
     WeightInitializer,
 )
-from axlearn.common.test_utils import TestCase, assert_allclose
+from axlearn.common.test_utils import TestCase, assert_allclose, set_threefry_partitionable
 
 
 class TestLayer(BaseLayer):
@@ -374,6 +374,7 @@ class BaseLayerTest(TestCase):
                 self.assertNestedAllClose(jnp.zeros_like(orig_value), noisy_value)
 
     @parameterized.parameters(False, True)
+    @set_threefry_partitionable(True)  # TODO(mhopkins): remove during jax 0.5.0+ upgrade
     def test_tensor_stats(self, inline_child_summaries: bool):
         test_layer: TestLayer = (
             TestLayer.default_config()
@@ -403,16 +404,16 @@ class BaseLayerTest(TestCase):
         if inline_child_summaries:
             self.assertNestedAllClose(
                 {
-                    "x": {"rms_norm": 9.327524, "max_abs": 26.052944},
-                    "y": {"rms_norm": 9.231870, "max_abs": 26.109497},
+                    "x": {"rms_norm": 9.478282, "max_abs": 26.26252},
+                    "y": {"rms_norm": 9.583241, "max_abs": 25.26252},
                 },
                 output_collections.summaries["tensor_stats"],
             )
         else:
             self.assertNestedAllClose(
                 {
-                    "x": {"norm": {"rms_norm": 9.327524}, "max": {"max_abs": 26.052944}},
-                    "y": {"norm": {"rms_norm": 9.231870}, "max": {"max_abs": 26.109497}},
+                    "x": {"norm": {"rms_norm": 9.478282}, "max": {"max_abs": 26.26252}},
+                    "y": {"norm": {"rms_norm": 9.583241}, "max": {"max_abs": 25.26252}},
                 },
                 output_collections.summaries["tensor_stats"],
             )
