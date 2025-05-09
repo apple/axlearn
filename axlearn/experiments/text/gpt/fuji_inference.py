f0a1c73de731698f0086b64bdb925251a8d3c35b
Hanzhi Zhou <hanzhi_zhou@apple.com>
2025-05-09 19:33:01 +0000

    fuji inference example

--- axlearn/experiments/text/gpt/fuji_inference.py
+++ axlearn/experiments/text/gpt/fuji_inference.py
@@ -1,9 +1,23 @@
+# Copyright © 2023 Apple Inc.
+
+"""An example to run offline inference for fuji.
+
+Some optimizations are ported from the not-yet open sourced inference engine so the forward pass
+performance is comparable to what we have internally.
+"""
+
+import functools
 import zlib
+from typing import Optional, Sequence
 
 import jax
 import jax.numpy as jnp
 
 from axlearn.common.attention import (
+    BaseLayer,
+    FusedGroupedQKVLinear,
+    GroupedQKVLinear,
+    Module,
     TransformerFeedForwardLayer,
     TransformerLayer,
     set_attention_partition_specs,
@@ -11,6 +25,7 @@ from axlearn.common.attention import (
 from axlearn.common.config import ConfigBase
 from axlearn.common.inference import DataPartitionType, InferenceRunner
 from axlearn.common.state_builder import Builder, TensorStoreStateStorageBuilder
+from axlearn.common.utils import Tensor
 from axlearn.experiments.text.gpt.common import MESH_AXIS_NAMES, mesh_shape_from_axes
 from axlearn.experiments.text.gpt.fuji import Version, get_trainer_kwargs
 
@@ -58,7 +73,7 @@ def set_inference_partition_spec(cfg: ConfigBase) -> ConfigBase:
 
 
 class DummyBuilder(TensorStoreStateStorageBuilder):
-    """A dummy state builder."""
+    """A dummy state builder that returns random weights."""
 
     def input_state_type(self) -> Builder.StateType:
         return Builder.StateType.TENSOR_SPECS
@@ -68,28 +83,104 @@ class DummyBuilder(TensorStoreStateStorageBuilder):
         seed = zlib.adler32(cfg.dir.encode("utf-8"))
 
         key = jax.random.PRNGKey(seed)
+        out_shardings = jax.tree.map(lambda spec: spec.sharding, state.trainer_state)
+
+        @functools.partial(jax.jit, out_shardings=out_shardings)
+        def jit_init():
+            return jax.tree.map(
+                lambda spec: jax.random.normal(key=key, shape=spec.shape, dtype=spec.dtype),
+                state.trainer_state,
+            )
+
         return Builder.State(
             step=0,
-            trainer_state=jax.tree.map(
-                lambda spec: jax.lax.with_sharding_constraint(
-                    jax.random.normal(key=key, shape=spec.shape, dtype=spec.dtype),
-                    shardings=spec.sharding,
-                )
-                if spec.dtype in [jnp.bfloat16, jnp.float32]
-                else jnp.zeros(shape=spec.shape, dtype=spec.dtype, device=spec.sharding),
-                state.trainer_state,
-            ),
+            trainer_state=jit_init(),
             built_keys=set(),
         )
 
 
-model_cfg = get_trainer_kwargs("test", vocab_size=32000, version=Version.V3, flash_attention=True)[
-    "model_cfg"
-]
+def replace_layer_config_recursively(
+    cfg: ConfigBase,
+    *,
+    target_cls: Module,
+    source_config: BaseLayer.Config,
+    exclude_keys: Optional[Sequence[str]] = None,
+) -> ConfigBase:
+    """Replaces the target_cls's config with the source_config.
+
+    This function is useful when one wants to replace a specific layer in the model, e.g.,
+    replacing MultiheadAttention layer with GroupedQueryAttention. Note that the target layer
+    should not be the root layer in the given model config.
+
+    Example usage -- Replacing MultiheadAttention by GroupedQueryAttention:
+        model_cfg = ... # The original model config.
+        replace_layer_config_recursively(
+            model_cfg,
+            target_cls=MultiheadAttention,
+            source_config=GroupedQueryAttention.default_config().set(num_kv_heads=8),
+            exclude_keys=["num_kv_heads"],
+        )
+
+    Args:
+        cfg: A ConfigBase, usually a top-level model config or a trainer config.
+        target_cls: A Module, the target layer class to be replaced.
+        source_config: A new BaseLayer config to be put into the model.
+        exclude_keys: A sequence of strings specifying which fields in the source config should
+           not be copied from the target config. By default, only klass is excluded.
+
+    Return:
+        A ConfigBase with the modified configs. This function also revises the input config in
+        place. So it is okay to not return anything.
+
+    Raises:
+        ValueError: If the target layer is the root of the input cfg.
+    """
+    if isinstance(cfg, target_cls.Config):
+        raise ValueError("The target cls cannot be the root of the input model config.")
+
+    exclude_kwargs = set(["klass"])
+    if exclude_keys is not None:
+        exclude_kwargs.update(exclude_keys)
+
+    def enter_fn(_, child, default_kv):
+        if isinstance(child, ConfigBase):
+            for key, value in child.items():
+                if isinstance(value, target_cls.Config):
+                    new_cfg = source_config.set(
+                        **{k: v for k, v in value.items() if k not in exclude_kwargs},
+                    )
+                    setattr(child, key, new_cfg)
+        return default_kv
+
+    cfg.visit(visit_fn=lambda k, v: None, enter_fn=enter_fn)
+    return cfg
+
+
+# Stop the generation early for profiling purposes.
+class LengthStopingCondition:
+    def __init__(self, length: int):
+        self._length = length
+
+    def __call__(self, *, index: Tensor, sequences: Tensor, out_of_prompt: Tensor) -> Tensor:
+        return jnp.broadcast_to((index >= self._length)[:, None], out_of_prompt.shape)
+
+
+# StackedTransformer is faster when doing inference.
+model_cfg = get_trainer_kwargs(
+    "70B", vocab_size=32000, version=Version.V3, flash_attention=True, use_stacked=True
+)["model_cfg"]
+# Groupde QKV linear has better sharding support.
+model_cfg = replace_layer_config_recursively(
+    model_cfg,
+    target_cls=FusedGroupedQKVLinear,
+    source_config=GroupedQKVLinear.default_config(),
+)
 model_cfg = set_inference_partition_spec(model_cfg)
-print(model_cfg)
+model_cfg.decoder.emb.token_emb.param_partition_spec = ("model", ("expert", "fsdp", "seq"))
+
+
 inference_runner_cfg = InferenceRunner.default_config().set(
-    mesh_shape=mesh_shape_from_axes(model=2),
+    mesh_shape=mesh_shape_from_axes(model=8),
     mesh_axis_names=MESH_AXIS_NAMES,
     model=model_cfg,
     inference_dtype=jnp.bfloat16,
@@ -97,25 +188,28 @@ inference_runner_cfg = InferenceRunner.default_config().set(
     init_state_builder=DummyBuilder.default_config().set(dir="dummy"),
     name="runner",
 )
+print(inference_runner_cfg)
 
 inference_runner = inference_runner_cfg.instantiate(parent=None)
 model = model_cfg.set(name="prefill_model").instantiate(parent=None)
 prng_key = jax.random.PRNGKey(0)
+stopping_cond = LengthStopingCondition(3)
+input_tokens = jnp.zeros((32, 4096), dtype=jnp.int32)
 
 
-def run_once(prng_key, tokens):
-    prng_key, subkey = jax.random.split(prng_key, 2)
-    r = inference_runner.run(
-        input_batches=[{"prefix": tokens}], method="sample_decode", prng_key=subkey
-    )
-    return list(r)[0], prng_key
+def input_iter():
+    # Warm up.
+    yield {"prefix": input_tokens}
+    jax.profiler.start_trace("/tmp/gpt_test/summaries")
+    yield {"prefix": input_tokens}
 
 
-input_tokens = jnp.zeros((1, 1024), dtype=jnp.int32)
-r, prng_key = run_once(prng_key, input_tokens)
-jax.block_until_ready(r)
+for r in inference_runner.run(
+    input_batches=input_iter(),
+    method="sample_decode",
+    prng_key=prng_key,
+    stop_decoding_condition=stopping_cond,
+):
+    jax.block_until_ready(r)
 
-jax.profiler.start_trace("/tmp/gpt_test/summaries")
-r, prng_key = run_once(prng_key, input_tokens)
-jax.block_until_ready(r)
 jax.profiler.stop_trace()
