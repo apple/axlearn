d22912b8355d1531453a3987a110227ef911c2b6
Jun Tian <juntian@apple.com>
2025-05-20 16:27:07 +0800

    Support GCS buckets with hierarchical namespace enabled

--- axlearn/common/file_system.py
+++ axlearn/common/file_system.py
@@ -9,6 +9,8 @@ This module provides a common interface between these options.
 """
 # TODO(markblee): Consider supporting a pathlib-like interface.
 
+import concurrent
+import concurrent.futures
 import contextlib
 import functools
 import os
@@ -86,6 +88,20 @@ def _gs_client():
     return storage.Client(project=gcp_settings("project"), credentials=get_credentials())
 
 
+@functools.cache
+def _gs_control_client():
+    """Creates or retrieves the google cloud storage control client.
+    Different from `_gs_client`, it provides metadata-specific and control plane operations .
+    """
+    # pylint: disable=import-outside-toplevel
+    # Avoid introducing a global dependency on `gcp`.
+    from google.cloud import storage_control_v2  # pytype: disable=import-error
+
+    from axlearn.cloud.gcp.utils import get_credentials
+
+    return storage_control_v2.StorageControlClient(credentials=get_credentials())
+
+
 @_wrap_tf_errors
 def isdir(path: str) -> bool:
     """Analogous to tf.io.gfile.isdir."""
@@ -95,6 +111,25 @@ def isdir(path: str) -> bool:
 @_wrap_tf_errors
 def listdir(path: str) -> list[str]:
     """Analogous to tf.io.gfile.listdir."""
+    parsed = urlparse(path)
+    if parsed.scheme == "gs":
+        bucket_name = parsed.netloc
+        prefix = parsed.path
+        if _is_hierarchical_namespace_enabled(bucket_name):
+            client = _gs_client()
+            blobs = client.list_blobs(
+                bucket_name,
+                prefix=prefix.strip("/") + "/",
+                delimiter="/",
+                include_folders_as_prefixes=True,
+            )
+            # objects
+            results = [os.path.basename(b.name) for b in blobs if not b.name.endswith("/")]
+            # folders
+            for p in blobs.prefixes:
+                results.append(os.path.basename(p.rstrip("/")) + "/")
+            return results
+
     return tf.io.gfile.listdir(path)
 
 
@@ -173,7 +208,59 @@ def makedirs(path: str):
     return tf.io.gfile.makedirs(path)
 
 
+@functools.cache
+def _is_hierarchical_namespace_enabled(bucket_name: str) -> bool:
+    """Return whether hierarchical namespace is enabled."""
+    client = _gs_client()
+    bucket = client.get_bucket(bucket_name)
+    return bucket.hierarchical_namespace_enabled
+
+
+def _rm_empty_folders(bucket: str, prefix: str):
+    """For a hierarchical namespace bucket, delete empty folders recursively."""
+    # pylint: disable=import-outside-toplevel
+    from google.cloud import storage_control_v2
+
+    client = _gs_control_client()
+    project_path = client.common_project_path("_")
+    bucket_path = f"{project_path}/buckets/{bucket}"
+    folders = set(
+        folder.name  # Format: "projects/{project}/buckets/{bucket}/folders/{folder}"
+        for folder in client.list_folders(
+            request=storage_control_v2.ListFoldersRequest(
+                parent=bucket_path, prefix=prefix.strip("/") + "/"
+            )
+        )
+    )
+
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        # Delete empty folders first, otherwise GCS will complain:
+        # FailedPrecondition: 400 The folder you tried to delete is not empty.
+        while len(folders) > 0:
+            parents = set(os.path.dirname(x.rstrip("/")) + "/" for x in folders)
+            leaves = folders - parents
+            res = list(
+                pool.map(
+                    client.delete_folder,
+                    [storage_control_v2.DeleteFolderRequest(name=f) for f in leaves],
+                )
+            )
+            folders = folders - leaves
+            logging.info(
+                "Deleted %s folders, %s remaining. [%s][%s]", len(res), len(folders), bucket, prefix
+            )
+
+
 @_wrap_tf_errors
 def rmtree(path: str):
-    """Analogous to tf.io.gfile.rmtree."""
-    return tf.io.gfile.rmtree(path)
+    """Analogous to tf.io.gfile.rmtree.
+    For a hierarchical namespace bucket, all the folders will also be removed."""
+    tf.io.gfile.rmtree(path)
+
+    # For HNS enabled bucket, we also need to delete folders recursively.
+    parsed = urlparse(path)
+    if parsed.scheme == "gs":
+        bucket_name = parsed.netloc
+        prefix = parsed.path
+        if _is_hierarchical_namespace_enabled(bucket_name):
+            _rm_empty_folders(bucket_name, prefix)
