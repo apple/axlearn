244868a0e319b8b9d59fe19886815cc69773e5c3
Sam Stoelinga <sammiestoel@gmail.com>
2025-05-30 15:29:32 -0700

    unlimited grpc messages

--- axlearn/cloud/gcp/jobset_utils.py
+++ axlearn/cloud/gcp/jobset_utils.py
@@ -453,6 +453,9 @@ class TPUReplicatedJob(SingleReplicatedJob):
 
         env_vars["TPU_PREMAPPED_BUFFER_SIZE"] = "137438953472"
         env_vars["TPU_PREMAPPED_BUFFER_TRANSFER_THRESHOLD_BYTES"] = "137438953472"
+        env_vars[
+            "TF_GRPC_DEFAULT_OPTIONS"
+        ] = "GRPC_ARG_MAX_SEND_MESSAGE_LENGTH=-1,GRPC_ARG_MAX_RECEIVE_MESSAGE_LENGTH=-1"
 
         resources = {"limits": {"google.com/tpu": system.chips_per_vm}}
         # Set request memory by host machine type.
