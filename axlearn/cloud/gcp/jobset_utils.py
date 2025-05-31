9ea0135bb3ccf404ee1cbf389350c6e219911949
Sam Stoelinga <sammiestoel@gmail.com>
2025-05-30 18:20:27 -0700

    custom libtpu

--- axlearn/cloud/gcp/jobset_utils.py
+++ axlearn/cloud/gcp/jobset_utils.py
@@ -453,6 +453,7 @@ class TPUReplicatedJob(SingleReplicatedJob):
 
         env_vars["TPU_PREMAPPED_BUFFER_SIZE"] = "137438953472"
         env_vars["TPU_PREMAPPED_BUFFER_TRANSFER_THRESHOLD_BYTES"] = "137438953472"
+        env_vars["TPU_LIBRARY_PATH"] = "/root/libtpu.so"
 
         resources = {"limits": {"google.com/tpu": system.chips_per_vm}}
         # Set request memory by host machine type.
