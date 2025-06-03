1575f9121ce1a7e5941ca666fa8fbf712e33f83e
Sam Stoelinga <sammiestoel@gmail.com>
2025-06-02 18:24:01 -0700

    add support for no orbax on small scale

--- test-orbax.sh
+++ test-orbax.sh
@@ -16,19 +16,22 @@ export PROJECT_ID=$(gcloud config get project)
 
 # The bundle step is needed if you run on cloudtop
 # uncomment if you use cloudtop
-# axlearn gcp bundle --name=$JOBSET_NAME \
-#         --bundler_spec=allow_dirty=True \
-#         --bundler_type=artifactregistry \
-#         --bundler_spec=dockerfile=Dockerfile \
-#         --bundler_spec=image=tpu \
-#         --bundler_spec=target=tpu
+axlearn gcp bundle --name=$JOBSET_NAME \
+        --bundler_spec=allow_dirty=True \
+        --bundler_type=artifactregistry \
+        --bundler_spec=dockerfile=Dockerfile \
+        --bundler_spec=image=tpu \
+        --bundler_spec=target=tpu
 
 # Only enable kueue when running on scale testing cluster
 # --queue=multislice-queue \
 # --priority_class=very-high \
 # --trainer_dir=gs://tess-checkpoints-us-west1/${JOBSET_NAME}-nr-${NUM_REPLICAS}/ \
 
-axlearn gcp launch run --cluster=$GKE_CLUSTER \
+# Check if CONFIG ends with "orbaxem"
+if [[ "$CONFIG" == *"orbaxem"* ]]; then
+  echo "Running with Orbax emergency checkpointer."
+  axlearn gcp launch run --cluster=$GKE_CLUSTER \
         --runner_name gke_tpu_single \
         --name=$JOBSET_NAME \
         --instance_type=${INSTANCE_TYPE} \
@@ -47,22 +50,24 @@ axlearn gcp launch run --cluster=$GKE_CLUSTER \
           --mesh_selector=${MESH_SELECTOR} \
           --initialization_timeout=1200 \
           --trace_at_steps=29,59,89,119,149,179,209,239,269,299,329,359,389,419,449,479,509,539,569,599,629,659,689,719
-# Non orbax
-# axlearn gcp launch run --cluster=bodaborg-v6e-256-tt-c \
-#         --runner_name gke_tpu_single \
-#         --name=$JOBSET_NAME \
-#         --queue=multislice-queue \
-#         --instance_type=tpu-v6e-256 \
-#         --priority_class=very-high \
-#         --num_replicas=${NUM_REPLICAS} \
-#         --bundler_spec=allow_dirty=True \
-#         --bundler_type=artifactregistry --bundler_spec=image=tpu \
-#         --bundler_spec=dockerfile=Dockerfile --bundler_spec=target=tpu \
-#         -- python3 -m axlearn.common.launch_trainer_main \
-#           --module=text.gpt.c4_trainer \
-#           --config=fuji-70B-v2-flash \
-#           --trainer_dir=gs://tess-checkpoints-us-west1/${JOBSET_NAME}-nr-${NUM_REPLICAS}/ \
-#           --data_dir=gs://axlearn-public/tensorflow_datasets  \
-#           --jax_backend=tpu \
-#           --mesh_selector=tpu-v6e-256-4 \
-#           --trace_at_steps=29,59,89,119,149,179,209,239,269,299,329,359,389,419,449,479,509,539,569,599,629,659,689,719
+
+else
+  echo "Running without Orbax emergency checkpointer."
+  axlearn gcp launch run --cluster=$GKE_CLUSTER \
+        --runner_name gke_tpu_single \
+        --name=$JOBSET_NAME \
+        --instance_type=${INSTANCE_TYPE} \
+        --num_replicas=${NUM_REPLICAS} \
+        --bundler_spec=allow_dirty=True \
+        --bundler_type=artifactregistry --bundler_spec=image=tpu \
+        --bundler_spec=dockerfile=Dockerfile --bundler_spec=target=tpu \
+        -- "python3 -c 'import jax; jax.devices()'; python3 -m axlearn.common.launch_trainer_main" \
+          --module=text.gpt.c4_trainer \
+          --config=${CONFIG} \
+          --trainer_dir=gs://${PROJECT_ID}-axlearn/${JOBSET_NAME}-nr-${NUM_REPLICAS}/ \
+          --data_dir=gs://axlearn-public/tensorflow_datasets  \
+          --jax_backend=tpu \
+          --mesh_selector=${MESH_SELECTOR} \
+          --initialization_timeout=1200 \
+          --trace_at_steps=29,59,89,119,149,179,209,239,269,299,329,359,389,419,449,479,509,539,569,599,629,659,689,719
+fi
