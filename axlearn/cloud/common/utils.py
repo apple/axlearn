4f2a3642feaab18bcbe710b5480bb523197b5b79
Haely Shah <haely@apple.com>
2025-06-06 13:31:22 -0700

    Add type

--- axlearn/cloud/common/utils.py
+++ axlearn/cloud/common/utils.py
@@ -292,7 +292,7 @@ def merge(base: dict, overrides: dict):
     return base
 
 
-def to_bool(value) -> bool:
+def to_bool(value: Any) -> bool:
     """Converts a string representation of truth to a bool."""
     if isinstance(value, bool):
         return value
