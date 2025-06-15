e68357652cdaf5357ca06f1240a06729fb1d3e41
Mark Lee <markblee@apple.com>
2025-06-14 19:27:43 -0700

    Revert "Do not materialize full text/image/speech logits (10GB for 16k seq text). (#1236)" (#1260)
    This reverts commit 2e3c00fdbb9af83b5bd1721d2bd3dcb37b5db6ea.

--- axlearn/common/decoder_test.py
+++ axlearn/common/decoder_test.py
@@ -109,23 +109,13 @@ class TestDecoder(TestCase):
 
         # Test values.
         def layer_output(state, layer):
-            prng_key = jax.random.PRNGKey(2)
-            outputs, _ = functional(
+            return functional(
                 layer,
                 inputs=dict(input_batch=dict(input_ids=inputs)),
                 state=state,
                 is_training=False,
-                prng_key=prng_key,
-            )
-            logits, _ = functional(
-                layer,
-                inputs=dict(forward_outputs=outputs),
-                state=state,
-                is_training=False,
-                prng_key=prng_key,
-                method="compute_logits",
-            )
-            return logits
+                prng_key=jax.random.PRNGKey(2),
+            )[0]["logits"]
 
         # Similarities with encoder_decoder_test.
         # pylint: disable=duplicate-code
@@ -208,23 +198,13 @@ class TestDecoder(TestCase):
 
             # Test values.
             def layer_output(state, layer):
-                prng_key = jax.random.PRNGKey(2)
-                outputs, _ = functional(
+                return functional(
                     layer,
                     inputs=dict(input_batch=dict(input_ids=input_ids)),
                     state=state,
                     is_training=False,
-                    prng_key=prng_key,
-                )
-                logits, _ = functional(
-                    layer,
-                    inputs=dict(forward_outputs=outputs),
-                    state=state,
-                    is_training=False,
-                    prng_key=prng_key,
-                    method="compute_logits",
-                )
-                return logits
+                    prng_key=jax.random.PRNGKey(2),
+                )[0]["logits"]
 
             ref_decoder_logits = layer_output(ref_decoder_state, ref_decoder)
             test_decoder_logits = layer_output(test_decoder_state, test_decoder)
@@ -414,7 +394,6 @@ class TestDecoder(TestCase):
                 )
                 * NEG_INF
             )
-        prng_key = jax.random.PRNGKey(0)
         forward_outputs, _ = functional(
             layer,
             inputs=dict(
@@ -428,15 +407,7 @@ class TestDecoder(TestCase):
             ),
             state=layer_params,
             is_training=False,
-            prng_key=prng_key,
-        )
-        fwd_logits, _ = functional(
-            layer,
-            inputs=dict(forward_outputs=forward_outputs),
-            state=layer_params,
-            is_training=False,
-            prng_key=prng_key,
-            method="compute_logits",
+            prng_key=jax.random.PRNGKey(0),
         )
 
         (initial_state, initial_outputs), _ = functional(
@@ -494,7 +465,7 @@ class TestDecoder(TestCase):
 
         # [batch, num_classes, tgt_len] --> [batch, tgt_len, num_classes].
         logits = jnp.moveaxis(logits, -1, -2)
-        assert_allclose(logits, fwd_logits)
+        assert_allclose(logits, forward_outputs["logits"])
 
     @parameterized.product(
         stack_cfg=[
@@ -729,15 +700,7 @@ class TestDecoder(TestCase):
                 state=layer_params,
                 prng_key=prng_key,
             )
-            fwd_logits = functional(
-                decoder,
-                inputs=dict(forward_outputs=outputs),
-                is_training=True,
-                prng_key=prng_key,
-                state=layer_params,
-                method="compute_logits",
-            )[0]
-            chex.assert_trees_all_close(fwd_logits, logits / temperature)
+            chex.assert_trees_all_close(outputs["logits"], logits / temperature)
 
             # Test prefill.
             (cached_states, prefill_outputs), _ = functional(
