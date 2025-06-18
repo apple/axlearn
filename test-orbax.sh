30b8f539c5c8e6cdf037be89cedac01e8c131feb
Sam Stoelinga <sammiestoel@gmail.com>
2025-06-18 08:37:58 -0700

    disable coredump

--- test-orbax.sh
+++ test-orbax.sh
@@ -18,12 +18,12 @@ export PROJECT_ID=$(gcloud config get project)
 
 # The bundle step is needed if you run on cloudtop
 # uncomment if you use cloudtop
-# axlearn gcp bundle --name=$JOBSET_NAME \
-#         --bundler_spec=allow_dirty=True \
-#         --bundler_type=artifactregistry \
-#         --bundler_spec=dockerfile=Dockerfile \
-#         --bundler_spec=image=tpu \
-#         --bundler_spec=target=tpu
+axlearn gcp bundle --name=$JOBSET_NAME \
+        --bundler_spec=allow_dirty=True \
+        --bundler_type=artifactregistry \
+        --bundler_spec=dockerfile=Dockerfile \
+        --bundler_spec=image=tpu \
+        --bundler_spec=target=tpu
 
 # Only enable kueue when running on scale testing cluster
 # --queue=multislice-queue \
@@ -39,10 +39,11 @@ if [[ "$CONFIG" == *"orbaxem"* ]]; then
         --instance_type=${INSTANCE_TYPE} \
         --host_mount_spec=name=tmp,host_path=/tmp,mount_path=/host-tmp \
         --num_replicas=${NUM_REPLICAS} \
+        --priority_class=very-high \
         --bundler_spec=allow_dirty=True \
         --bundler_type=artifactregistry --bundler_spec=image=tpu \
         --bundler_spec=dockerfile=Dockerfile --bundler_spec=target=tpu \
-        -- "ulimit -n 1048576; python3 -c 'import jax; jax.devices()'; python3 -m axlearn.common.launch_trainer_main" \
+        -- "ulimit -n 1048576; ulimit -c 0; python3 -c 'import jax; jax.devices()'; python3 -m axlearn.common.launch_trainer_main" \
           --init_module=axlearn.common.checkpointer_orbax_emergency:local_ckpt_dir=/host-tmp/checkpoints \
           --module=text.gpt.c4_trainer \
           --config=${CONFIG} \
