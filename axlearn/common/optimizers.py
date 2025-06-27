313f8da1f738484f91b8eefee910b116b2d77431
reed_li <reed.lee.1988@gmail.com>
2025-06-27 15:28:11 +0800

    Update axlearn/common/optimizers.py
    Co-authored-by: Ruoming Pang <ruoming@gmail.com>

--- axlearn/common/optimizers.py
+++ axlearn/common/optimizers.py
@@ -2022,7 +2022,7 @@ def adastar_optimizer(
             params, per_param_scale=weight_decay_per_param_scale
         )
         updates2 = jax.tree.map(
-            lambda u, p, s: None if u is None else _update2(u, param=p, weight_decay_scale=s),
+            lambda u, p, wds: None if u is None else _update2(u, param=p, weight_decay_scale=wds),
             updates,
             params,
             weight_decay_scales,
