130650c0853088e0a65cbf099eec5980adc88043
Edwinhr716 <Edandres249@gmail.com>
2025-06-29 23:54:27 +0000

    cleanup

--- axlearn/cloud/gcp/pathways_utils.py
+++ axlearn/cloud/gcp/pathways_utils.py
@@ -985,7 +985,7 @@ class PathwaysLeaderWorkerTemplate(BaseLeaderWorkerTemplate):
 
         return dict(
             name=cfg.name,
-            image=self._bundler.id(cfg.name),
+            image="",
             command=["bash", "-c", cfg.command],
             imagePullPolicy="Always",
         )
