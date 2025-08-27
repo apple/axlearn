1bdb6bb02ab853382a480f3ca4a9afc5ef366518
Sam Stoelinga <sammiestoel@gmail.com>
2025-08-26 17:53:22 -0700

    set max step to 30

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -1044,7 +1044,7 @@ def trainer_configs(
             ),
             **kwargs,
         )
-        config_map[config_name] = with_overrides(config_map[config_name], max_step=15)
+        config_map[config_name] = with_overrides(config_map[config_name], max_step=30)
 
         def make_fp8_config(base_config_name: str) -> SpmdTrainer.Config:
             """Make a FP8 variant of the base config.
