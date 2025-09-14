8dc5205d902fe05ff70858a37ec7c8d4975a9503
Sam Stoelinga <sammiestoel@gmail.com>
2025-09-13 22:58:13 -0700

    stick to default concurrent restore of 32gb

--- axlearn/common/array_serialization.py
+++ axlearn/common/array_serialization.py
@@ -618,10 +618,10 @@ class GlobalAsyncCheckpointManager(serialization.GlobalAsyncCheckpointManager):
         tensorstore_specs: Sequence[dict[str, Any]],
         global_shapes: Optional[Sequence[array.Shape]] = None,
         dtypes: Optional[Sequence[typing.DTypeLike]] = None,
-        concurrent_gb: int = 128,
+        concurrent_gb: int = 32,
     ):
         # force to 128
-        concurrent_gb = max(128, concurrent_gb)
+        # concurrent_gb = max(128, concurrent_gb)
         logging.info("concurrent_gb=%s GB.", concurrent_gb)
         self.wait_until_finished()
         start_time = time.time()
