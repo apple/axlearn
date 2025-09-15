09c5bff4f41f073b9f51eb063a7fbea9296ada6c
Sam Stoelinga <sammiestoel@gmail.com>
2025-09-15 14:00:11 -0700

    fix training script

--- train_fuji_7b.sh
+++ train_fuji_7b.sh
@@ -1,7 +1,10 @@
+
+# --trainer_dir=gs://cloud-tpu-multipod-dev-uss1/axlearn-fuji-v2-7b/ \
+
 python3 -m axlearn.common.launch_trainer_main \
         --module=text.gpt.c4_trainer \
         --config=fuji-7B-v2-flash \
-        --trainer_dir=gs://cloud-tpu-multipod-dev-uss1/axlearn-fuji-v2-7b/ \
+        --trainer_dir=gs://cloud-tpu-multipod-dev-euw4/axlearn-fuji-v2-7b/ \
         --data_dir=gs://axlearn-public/tensorflow_datasets  \
         --jax_backend=proxy \
         --mesh_selector=tpu-v5litepod-32-1
