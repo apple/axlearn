6792b25b7675a4f04c0e518e51ff7415dab29063
Sam Stoelinga <sammiestoel@gmail.com>
2025-09-16 11:21:38 -0700

    train fuji 7b on v5p / restore

--- train_fuji_7b.sh
+++ train_fuji_7b.sh
@@ -7,4 +7,4 @@ python3 -m axlearn.common.launch_trainer_main \
         --trainer_dir=gs://cloud-tpu-multipod-dev-euw4/axlearn-fuji-v2-7b/ \
         --data_dir=gs://axlearn-public/tensorflow_datasets  \
         --jax_backend=proxy \
-        --mesh_selector=tpu-v5litepod-32-1
+        --mesh_selector=tpu-v5p
